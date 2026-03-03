7badb2c516edda67e821b453d1116d50ba4d6fac
awei <2069682479@qq.com>
2026-03-03 16:41:57 +0800

    提交Java架构

---
+++ eams-java/eams-j3-class/src/main/java/com/homework/eamsj3class/EamsJ3ClassApplication.java
@@ -0,0 +1,13 @@
+package com.homework.eamsj3class;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EamsJ3ClassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EamsJ3ClassApplication.class, args);
+    }
+
+}
