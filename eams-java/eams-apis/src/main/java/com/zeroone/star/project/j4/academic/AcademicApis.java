2a4c0eb5a1f25ffc1b10320387ed328e49700ae6
Ringo0115 <2752649921@qq.com>
2026-03-14 15:56:28 +0800

    j4小组模块第二次初始化,补充apis模块的源文件

---
+++ eams-java/eams-apis/src/main/java/com/zeroone/star/project/j4/academic/AcademicApis.java
@@ -0,0 +1,4 @@
+package com.zeroone.star.project.j4.academic;
+
+public interface AcademicApis {
+}
---
+++ eams-java/eams-apis/src/main/java/com/zeroone/star/project/j4/student/StudentApis.java
@@ -0,0 +1,4 @@
+package com.zeroone.star.project.j4.student;
+
+public interface StudentApis {
+}
