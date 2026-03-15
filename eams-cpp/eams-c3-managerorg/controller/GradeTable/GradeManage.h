42d29078a291d4653feef76b90cc0989dba11987
Liu <1992869702@qq.com>
2026-03-15 19:38:19 +0800

    添加导入成绩功能的endpoint

--- eams-cpp/eams-c3-managerorg/controller/GradeTable/GradeManage.h
+++ eams-cpp/eams-c3-managerorg/controller/GradeTable/GradeManage.h
@@ -15,6 +15,13 @@ class GradeManage: public oatpp::web::server::api::ApiController
 	API_ACCESS_DECLARE(GradeManage);
 public:		//定义接口
 
+	//// 3.1 定义导入接口描述
+	//API_DEF_ENDPOINT_INFO_FILE_AUTH(ZH_WORDS_GETTER("sample.import.summary"), importGrade, SampleImportDTO::Wrapper, StringJsonVO::Wrapper, API_TAG);
+	//// 3.2 定义导入接口处理
+	//API_HANDLER_ENDPOINT_AUTH(API_M_POST, "/c3/org/Grade", importGrade, REQUEST(std::shared_ptr<IncomingRequest>, request), execImportGrade(request, authObject->getPayload()));
+
+
+
 private:	//定义接口执行函数
 
 };
