8e343002df9264572b012ea8c0e2d12d71b955c1
While <2981340977@qq.com>
2026-03-15 19:41:38 +0800

    添加一下control，VO，DTO，Query，以及api的订阅 1. 获取消课记录 2. 获取积分记录 3. 调整积分,

---
+++ eams-java/eams-domain/src/main/java/com/zeroone/star/project/dto/j8/stumanager/common/AdjustPointsDTO.java
@@ -0,0 +1,27 @@
+﻿package com.zeroone.star.project.dto.j8.stumanager.common;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * 调整积分参数
+ */
+@Data
+@ApiModel("调整积分参数")
+public class AdjustPointsDTO {
+    @NotNull(message = "学员ID不能为空")
+    @ApiModelProperty(value = "学员ID", example = "1", required = true)
+    private Long studentId;
+
+    @NotNull(message = "调整积分不能为空")
+    @ApiModelProperty(value = "调整积分，正数为增加，负数为扣减", example = "10", required = true)
+    private Integer changeCredit;
+
+    @NotBlank(message = "调整原因不能为空")
+    @ApiModelProperty(value = "调整原因", example = "活动奖励", required = true)
+    private String remark;
+}
