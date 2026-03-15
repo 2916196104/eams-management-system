98dd5c40782d2a60a56e418c184959af1bcdfb50
tsfmn <2730747878@qq.com>
2026-03-15 16:24:33 +0800

    创建组员分支增加接口和领域模型

--- eams-java/eams-apis/src/main/java/com/zeroone/star/project/j8/stumanager/common/StuInformationApis.java
+++ eams-java/eams-apis/src/main/java/com/zeroone/star/project/j8/stumanager/common/StuInformationApis.java
@@ -1,7 +1,22 @@
 package com.zeroone.star.project.j8.stumanager.common;
+import com.zeroone.star.project.dto.j8.SaveStu.SaveStuDTO;
+import com.zeroone.star.project.dto.j8.SaveStu.StuSignCourseDTO;
+import com.zeroone.star.project.vo.JsonVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
+@Api(tags = "学员管理")
 /**
  * 学员信息
  */
 public interface StuInformationApis {
+
+    @ApiOperation("保存学员")
+    JsonVO<Void> saveStudent();
+
+    @ApiOperation("学员阶段设置")
+    JsonVO<Void> setStudentStage();
+
+    @ApiOperation("学员报名课程")
+    JsonVO<Void> enrollCourse();
 }
