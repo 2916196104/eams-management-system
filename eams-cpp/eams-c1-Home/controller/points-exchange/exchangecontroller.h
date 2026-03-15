67d22a1752ed69f014b4e153dcfcbdf99d7223ee
SW <16148164+wyswsws@user.noreply.gitee.com>
2026-03-15 15:17:26 +0800

    创建c1小组框架

---
+++ eams-cpp/eams-c1-Home/controller/points-exchange/exchangecontroller.h
@@ -0,0 +1,44 @@
+#pragma once
+/*
+ Copyright Zero One Star. All rights reserved.
+
+ @Author: awei
+ @Date: 2026/03/08 21:10:30
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+	  https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+#ifndef _EXCHANGECONTRULLER_H_
+#define _EXCHANGECONTROLLER_H_
+
+#include "ApiHelper.h"
+#include "ServerInfo.h"
+#include "Macros.h"
+#include "domain/vo/BaseJsonVO.h"
+#include "domain/query/PageQuery.h"
+
+#include OATPP_CODEGEN_BEGIN(ApiController)
+//积分兑换模块控制器
+class exchangeController : public oatpp::web::server::api::ApiController
+{
+	// 定义控制器访问入口
+	API_ACCESS_DECLARE(exchangeController);
+public: // 定义接口
+
+
+private: // 定义接口执行函数
+
+};
+
+#include OATPP_CODEGEN_END(ApiController)
+
+#endif // !_EXCHANGECONTROLLER_H_
