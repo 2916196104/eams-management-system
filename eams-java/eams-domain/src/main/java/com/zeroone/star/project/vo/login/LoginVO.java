501c058d57ac7e63ae13a065a947e73cf74cdbdb
melody <1340135887@qq.com>
2026-03-16 13:44:32 +0800

    feat: 补齐登录模块接口与领域模型

--- eams-java/eams-domain/src/main/java/com/zeroone/star/project/vo/login/LoginVO.java
+++ eams-java/eams-domain/src/main/java/com/zeroone/star/project/vo/login/LoginVO.java
@@ -8,25 +8,26 @@ import java.util.List;
 
 /**
  * <p>
- * 描述：登录显示数据对象
+ * 描述：当前登录用户展示对象
  * </p>
  * <p>版权：&copy;01星球</p>
  * <p>地址：01星球总部</p>
+ *
  * @author 阿伟学长
  * @version 1.0.0
  */
-@ApiModel("登录显示对象")
+@ApiModel("当前登录用户展示对象")
 @Data
 public class LoginVO {
-    @ApiModelProperty(value = "用户唯一编号", example = "1")
+    @ApiModelProperty(value = "用户编号", example = "1")
     private String id;
 
     @ApiModelProperty(value = "用户名", example = "admin")
     private String username;
 
-    @ApiModelProperty(value = "是否启用 1 启用 0 禁用 ", example = "1")
+    @ApiModelProperty(value = "是否启用，1启用，0禁用", example = "1")
     private Byte isEnabled;
 
-    @ApiModelProperty(value = "用户角色列表", example = "['ADMIN','MANAGER']")
+    @ApiModelProperty(value = "角色列表", example = "[\"ADMIN\"]")
     private List<String> roles;
 }
