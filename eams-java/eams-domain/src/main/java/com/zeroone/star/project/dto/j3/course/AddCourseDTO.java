6568de3764eaf03920f376488cb8a888cea95ea3
chenhe <57572703@gitlab.com>
2026-03-16 16:35:05 +0800

    修改swagger name->value

--- eams-java/eams-domain/src/main/java/com/zeroone/star/project/dto/j3/course/AddCourseDTO.java
+++ eams-java/eams-domain/src/main/java/com/zeroone/star/project/dto/j3/course/AddCourseDTO.java
@@ -16,46 +16,46 @@ import lombok.Data;
 @AllArgsConstructor
 @ApiModel("添加课程数据DTO")
 public class AddCourseDTO {
-    @ApiModelProperty(name = "课程名称", example = "测试课程2")
+    @ApiModelProperty(value = "课程名称", example = "测试课程2")
     String name;
-    @ApiModelProperty(name = "科目Id", example = "17")
+    @ApiModelProperty(value = "科目Id", example = "17")
     Long subjectId;
-    @ApiModelProperty(name = "科目", example = "书法")
+    @ApiModelProperty(value = "科目", example = "书法")
     String subjectName;
-    @ApiModelProperty(name = "适用年级Id", example = "4")
+    @ApiModelProperty(value = "适用年级Id", example = "4")
     String gradeIds;
-    @ApiModelProperty(name = "适用年级", example = "2026级")
+    @ApiModelProperty(value = "适用年级", example = "2026级")
     String gradeNames;
-    @ApiModelProperty(name = "单位", example = "期")
+    @ApiModelProperty(value = "单位", example = "期")
     String unitName;
-    @ApiModelProperty(name = "总价", example = "100.00")
+    @ApiModelProperty(value = "总价", example = "100.00")
     String price;
-    @ApiModelProperty(name = "课次数", example = "15")
+    @ApiModelProperty(value = "课次数", example = "15")
     Integer lessonCount;
-    @ApiModelProperty(name = "有效月数", example = "12")
+    @ApiModelProperty(value = "有效月数", example = "12")
     Integer expireMonths;
-    @ApiModelProperty(name = "课程类型", example = "一对一")
+    @ApiModelProperty(value = "课程类型", example = "一对一")
     String lessonType;
-    @ApiModelProperty(name = "消课课酬", example = "0")
+    @ApiModelProperty(value = "消课课酬", example = "0")
     String salary;
-    @ApiModelProperty(name = "预约模式", example = "true")
+    @ApiModelProperty(value = "预约模式", example = "true")
     Boolean bookable;
-    @ApiModelProperty(name = "是否上架", example = "是")
+    @ApiModelProperty(value = "是否上架", example = "是")
     Boolean forSale;
-    @ApiModelProperty(name = "课程封面图", example = "http://localhost:80/uploads/2026-03-10/48a6d934-b1ba-43b7-84a8-5e2ac20b9fc6.jpg")
+    @ApiModelProperty(value = "课程封面图", example = "http://localhost:80/uploads/2026-03-10/48a6d934-b1ba-43b7-84a8-5e2ac20b9fc6.jpg")
     String cover;
-    @ApiModelProperty(name = "精品推荐", example = "是")
+    @ApiModelProperty(value = "精品推荐", example = "是")
     Boolean recommend;
-    @ApiModelProperty(name = "库存量", example = "100")
+    @ApiModelProperty(value = "库存量", example = "100")
     Integer storage;
-    @ApiModelProperty(name = "关联数", example = "1")
+    @ApiModelProperty(value = "关联数", example = "1")
     Integer linkCount;
-    @ApiModelProperty(name = "停售日期", example = "2026-05-01")
+    @ApiModelProperty(value = "停售日期", example = "2026-05-01")
     String closeDate;
-    @ApiModelProperty(name = "详情介绍图集", example = "http://localhost:80/uploads/2026-03-10/48a6d934-b1ba-43b7-84a8-5e2ac20b9fc6.jpg")
+    @ApiModelProperty(value = "详情介绍图集", example = "http://localhost:80/uploads/2026-03-10/48a6d934-b1ba-43b7-84a8-5e2ac20b9fc6.jpg")
     String images;
-    @ApiModelProperty(name = "师资说明", example = "测试师资说明")
+    @ApiModelProperty(value = "师资说明", example = "测试师资说明")
     String teacher_info;
-    @ApiModelProperty(name = "服务说明", example = "测试服务说明")
+    @ApiModelProperty(value = "服务说明", example = "测试服务说明")
     String service_info;
 }
--- eams-java/eams-domain/src/main/java/com/zeroone/star/project/vo/j3/course/CourseListVO.java
+++ eams-java/eams-domain/src/main/java/com/zeroone/star/project/vo/j3/course/CourseListVO.java
@@ -16,39 +16,39 @@ import lombok.Data;
 @AllArgsConstructor
 @ApiModel("分页展示课程信息DTO")
 public class CourseListVO {
-    @ApiModelProperty(name = "课程ID", example = "2")
+    @ApiModelProperty(value = "课程ID", example = "2")
     Long id;
-    @ApiModelProperty(name = "课程名称", example = "测试课程2")
+    @ApiModelProperty(value = "课程名称", example = "测试课程2")
     String name;
-    @ApiModelProperty(name = "科目Id", example = "17")
+    @ApiModelProperty(value = "科目Id", example = "17")
     Long subjectId;
-    @ApiModelProperty(name = "科目", example = "书法")
+    @ApiModelProperty(value = "科目", example = "书法")
     String subjectName;
-    @ApiModelProperty(name = "课型", example = "一对一")
+    @ApiModelProperty(value = "课型", example = "一对一")
     String lessonType;
-    @ApiModelProperty(name = "单次价", example = "7.00")
+    @ApiModelProperty(value = "单次价", example = "7.00")
     String unitPrice;
-    @ApiModelProperty(name = "总售价", example = "100.00")
+    @ApiModelProperty(value = "总售价", example = "100.00")
     String price;
-    @ApiModelProperty(name = "课次数", example = "15")
+    @ApiModelProperty(value = "课次数", example = "15")
     Integer lessonCount;
-    @ApiModelProperty(name = "单位", example = "期")
+    @ApiModelProperty(value = "单位", example = "期")
     String unitName;
-    @ApiModelProperty(name = "关联数", example = "1")
+    @ApiModelProperty(value = "关联数", example = "1")
     Integer linkCount;
-    @ApiModelProperty(name = "可预约", example = "true")
+    @ApiModelProperty(value = "可预约", example = "true")
     Boolean bookable;
-    @ApiModelProperty(name = "上架状态", example = "是")
+    @ApiModelProperty(value = "上架状态", example = "是")
     String forSale;
-    @ApiModelProperty(name = "库存", example = "100")
+    @ApiModelProperty(value = "库存", example = "100")
     Integer storage;
-    @ApiModelProperty(name = "销售量", example = "0")
+    @ApiModelProperty(value = "销售量", example = "0")
     Integer saleCount;
-    @ApiModelProperty(name = "销售额", example = "0.00")
+    @ApiModelProperty(value = "销售额", example = "0.00")
     Integer saleAmount;
-    @ApiModelProperty(name = "销售截至", example = "2026-05-01")
+    @ApiModelProperty(value = "销售截至", example = "2026-05-01")
     String closeDate;
-    @ApiModelProperty(name = "课程状态", example = "启用")
+    @ApiModelProperty(value = "课程状态", example = "启用")
     String state;
 
 }
