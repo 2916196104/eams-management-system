517eac411a180da190061ec2b6ee032de46b6180
TrAveller7150 <1427998531@qq.com>
2026-03-16 19:19:17 +0800

    增加一些配置

--- eams-java/eams-j3-supplies/src/main/java/com/homework/eamsj3supplies/controller/SuppliesController.java
+++ eams-java/eams-j3-supplies/src/main/java/com/homework/eamsj3supplies/controller/SuppliesController.java
@@ -9,6 +9,7 @@ import com.zeroone.star.project.vo.j3.supplies.MaterialRecordVO;
 import com.zeroone.star.project.vo.j3.supplies.MaterialStockVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +29,7 @@ public class SuppliesController implements SuppliesApis {
         return null;
     }
 
+    @GetMapping("/records")
     @Override
     @ApiOperation("获取物料出入库记录列表")
     public JsonVO<PageDTO<MaterialRecordVO>> queryMaterialRecordPage(MaterialRecordQuery query) {
