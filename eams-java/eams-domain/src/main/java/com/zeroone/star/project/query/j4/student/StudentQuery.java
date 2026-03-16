a01748985dcee3bc81e33dbe0bca5383b1984e93
chenze5 <chenze5@asiainfo.com>
2026-03-16 12:43:39 +0800

    1.新增课表统计的query（StudentQuery） 2.新增课表统计的VO（CourseCounterVO）

--- eams-java/eams-domain/src/main/java/com/zeroone/star/project/query/j4/student/StudentQuery.java
+++ eams-java/eams-domain/src/main/java/com/zeroone/star/project/query/j4/student/StudentQuery.java
@@ -1,4 +1,12 @@
 package com.zeroone.star.project.query.j4.student;
+
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.Tag;
+import lombok.Data;
+
 //StudentQuery: 包含姓名、电话、状态（在线/意向/结业）、顾问ID、分页参数。
+@Data
 public class StudentQuery {
+    @ApiModelProperty(value = "学员ID", example = "2008418408985583617")
+    long StudentId;
 }
