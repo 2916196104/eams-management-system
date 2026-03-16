f18071b14ce215875962431b3217dc1389347ab7
aliyun8300553656 <accounts_69ad2a5ed2a2c377c5148c15@mail.teambition.com>
2026-03-16 13:07:42 +0800

    更新 InternalNoticeApis.java

--- eams-java/eams-apis/src/main/java/com/zeroone/star/project/j8/sysmanager/InternalNoticeApis.java
+++ eams-java/eams-apis/src/main/java/com/zeroone/star/project/j8/sysmanager/InternalNoticeApis.java
@@ -3,5 +3,10 @@ package com.zeroone.star.project.j8.sysmanager;
 /**
  * 内部公告
  */
+@Api(tags = "内部公告")
+
 public interface InternalNoticeApis {
+    @ApiOperation("删除内部公告")
+    JsonVO<PageDTO<noticeListDTO>> deleteSysNotice();
 }
+
