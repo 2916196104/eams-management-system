16e8656aca08fb6bc8f28b14842a54abad20d2b4
FLIN <accounts_69ad00afd2a2c377c5134a2a@mail.teambition.com>
2026-03-17 18:41:09 +0800

    更新 ClassStudentDTO.java

--- eams-java/eams-domain/src/main/java/com/zeroone/star/project/dto/j5/classmanager/ClassStudentDTO.java
+++ eams-java/eams-domain/src/main/java/com/zeroone/star/project/dto/j5/classmanager/ClassStudentDTO.java
@@ -12,10 +12,44 @@ import java.util.List;
 @Data
 @ApiModel(value = "班级学生传输对象", description = "往班级添加学员的请求参数")
 public class ClassStudentDTO {
-    @ApiModelProperty(value = "加入的班级id", required = true, example = "1")
-    private int classId;
+    @ApiModelProperty(value = "班级id,如果保存必须传，新增不传")
+    private Long id;
 
-    @ApiModelProperty(value = "添加的所有学生的id", required = true, example = "1,2,3")
-    private List<Integer> studentIds;
+    @ApiModelProperty(value = "班级名",example = "1班",required = true)
+    private String name;
+
+    @ApiModelProperty(value = "分校校区",example = "1校区",required = true)
+    private String school;
+
+    @ApiModelProperty(value = "年级",example = "1年级")
+    private String grade;
+
+    @ApiModelProperty(value = "班主任",example = "张三",required = true)
+    private String teacher;
+
+    @ApiModelProperty(value = "班级预招人数",example = "10",required = true)
+    private Integer number;
+
+    @ApiModelProperty(value = "预排课次数",example = "10",required = true)
+    private Integer courseCount;
+
+    @ApiModelProperty(value = "默认课程",example = "1课程")
+    private String defaultCourse;
+
+    @ApiModelProperty(value = "默认教室",example = "111")
+    private String room;
+
+
+    @ApiModelProperty(value = "班级创建时间",example = "2021-01-01 00:00:00")
+    private String createTime;
+
+    @ApiModelProperty(value = "班级结束时间",example = "2025-01-01 00:00:00")
+    private String updateTime;
+
+    @ApiModelProperty(value = "排课备注",example = "比别的课程晚两个礼拜开课")
+    private String description;
+
+    @ApiModelProperty(value = "班级状态",example = "已结业")
+    private String status;
 
 }
