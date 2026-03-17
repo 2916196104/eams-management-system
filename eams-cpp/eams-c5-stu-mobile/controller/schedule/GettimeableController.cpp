fcdfd9507cabed875d5fbd57792af1d18a9acc97
a1525 <a1525@pandaafter.com>
2026-03-17 21:48:40 +0800

    获取课表接口中文词典添加

--- eams-cpp/eams-c5-stu-mobile/controller/schedule/GettimeableController.cpp
+++ eams-cpp/eams-c5-stu-mobile/controller/schedule/GettimeableController.cpp
@@ -1,7 +1,7 @@
 
 #include"stdafx.h"
 #include"GettimeableController.h"
-StringJsonVO::Wrapper GettimeableController::executequerytest(const PageQuery::Wrapper & query)
+StringJsonVO::Wrapper GettimeableController::executequerygettimeable(const PageQuery::Wrapper & query)
 {
 	//创建一个响应结果
 	auto vo = StringJsonVO::createShared();
--- eams-cpp/eams-c5-stu-mobile/controller/schedule/GettimeableController.h
+++ eams-cpp/eams-c5-stu-mobile/controller/schedule/GettimeableController.h
@@ -16,7 +16,7 @@ class GettimeableController : public oatpp::web::server::api::ApiController {
     API_ACCESS_DECLARE(GettimeableController);
 public://定义接口
     ENDPOINT_INFO(querytest) {
-        info->summary = "get timeable";//暂时这么搞，中文词典后期要统一
+        info->summary = ZH_WORDS_GETTER("gettimeable.summary");//暂时这么搞，中文词典后期要统一
         //支持授权
         API_DEF_ADD_AUTH();
         // 定义响应参数格式
@@ -24,20 +24,20 @@ public://定义接口
         // 定义请求参数格式
         API_DEF_ADD_PAGE_PARAMS();
         //asd之后需要加入中文词典
-        API_DEF_ADD_QUERY_PARAMS(String,"teacher_name", "asd", "li", false);
-        API_DEF_ADD_QUERY_PARAMS(String, "time", "asd", "Friday", false);
+        API_DEF_ADD_QUERY_PARAMS(String, "name", ZH_WORDS_GETTER("gettimeable.search1"), ZH_WORDS_GETTER("course.name1"), false);
+        API_DEF_ADD_QUERY_PARAMS(String,"teacher_info", ZH_WORDS_GETTER("gettimeable.search2"), "Roin"+ZH_WORDS_GETTER("course.teachername1"), false);
     }
     //接口等待后期更改
-    ENDPOINT(API_M_GET, "/test", querytest, QUERIES(QueryParams, params),API_HANDLER_AUTH_PARAME){
+    ENDPOINT(API_M_GET, "/gettimeable", querytest, QUERIES(QueryParams, params),API_HANDLER_AUTH_PARAME){
         //解析查询参数
         API_HANDLER_QUERY_PARAM(query, PageQuery, params);
         //响应结果
-        API_HANDLER_RESP_VO(executequerytest(query));
+        API_HANDLER_RESP_VO(executequerygettimeable(query));
 
     }
     // TODO - more endpoints here
 private://定义接口执行函数
-    StringJsonVO::Wrapper executequerytest(const PageQuery::Wrapper& query);
+    StringJsonVO::Wrapper executequerygettimeable(const PageQuery::Wrapper& query);
 };
 
 #include OATPP_CODEGEN_END(ApiController)
