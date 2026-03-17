3ec44ac4219cb1e6fb20a631c2c6133d58d1635a
ZXY-344 <1434417697@qq.com>
2026-03-17 20:13:20 +0800

    恢复了之前误删的内容

--- eams-cpp/eams-c6-student/controller/Router.cpp
+++ eams-cpp/eams-c6-student/controller/Router.cpp
@@ -23,11 +23,13 @@
 
 #include "summaryclass/SummaryClassController.h"
 #include "parentaccount/ParentAccountController.h"
-
+// 如果定义了关闭Swagger文档宏
 #ifdef CLOSE_SWAGGER_DOC
+// 简化绑定控制器宏定义
 #define ROUTER_SIMPLE_BIND(__CLASS__) \
 router->addController(__CLASS__::createShared())
 #else
+// 简化绑定控制器宏定义
 #define ROUTER_SIMPLE_BIND(__CLASS__) \
 BIND_CONTROLLER(docEndpoints, router, __CLASS__)
 #endif
