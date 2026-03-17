241bb8097b77a7fa8662f21a0275b413c38e9b24
Dline666 <2787377794@qq.com>
2026-03-17 14:25:22 +0800

    编写了领域模型和控制器

--- eams-cpp/eams-c1-Home/controller/listening-card/cardcontroller.h
+++ eams-cpp/eams-c1-Home/controller/listening-card/cardcontroller.h
@@ -25,18 +25,32 @@
 #include "Macros.h"
 #include "domain/vo/BaseJsonVO.h"
 #include "domain/query/PageQuery.h"
+#include "domain/vo/listening-card/cardVO.h"
+#include "domain/query/listening-card/cardquery.h"
+
 
 #include OATPP_CODEGEN_BEGIN(ApiController)
+#define API_TAG ZH_WORDS_GETTER("home.listening-card.tag")
+
 //试听卡模块控制器
 class cardController : public oatpp::web::server::api::ApiController
 {
 	// 定义控制器访问入口
 	API_ACCESS_DECLARE(cardController);
-public: // 定义接口
 
+	// 定义获取试听卡接口描述
+	API_DEF_ENDPOINT_INFO_AUTH(ZH_WORDS_GETTER("home.listening-card.get.list"), cardQuery,cardListJsonVO::Wrapper, API_TAG);
+	// 定义获取试听卡接口端点
+	API_HANDLER_ENDPOINT_NOPARAM_AUTH(API_M_GET, "/c1/home/card-list", cardQuery, executeCardListQuery());
 
-private: // 定义接口执行函数
+	// 定义获取试听卡规则接口描述
+	API_DEF_ENDPOINT_INFO_AUTH(ZH_WORDS_GETTER("home.listening-card.get.rule"), cardRuleQuery, cardRuleJsonVO::Wrapper, API_TAG);
+	// 定义获取试听卡规则接口端点
+	API_HANDLER_ENDPOINT_NOPARAM_AUTH(API_M_GET, "/c1/home/card-rule", cardRuleQuery, executeCardRuleQuery());
 
+private:
+	cardListJsonVO::Wrapper executeCardListQuery();
+	cardRuleJsonVO::Wrapper executeCardRuleQuery();
 };
 
 #include OATPP_CODEGEN_END(ApiController)
