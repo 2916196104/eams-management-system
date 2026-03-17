c945c4ea5153fcc4222b86349a1ff55b7f326148
Kiana2233 <1661624270@qq.com>
2026-03-17 17:23:42 +0800

    修改顾问接口、导入导出学员接口

--- eams-java/eams-apis/src/main/java/com/zeroone/star/project/j4/student/StudentApis.java
+++ eams-java/eams-apis/src/main/java/com/zeroone/star/project/j4/student/StudentApis.java
@@ -2,10 +2,32 @@ package com.zeroone.star.project.j4.student;
 
 import com.zeroone.star.project.dto.PageDTO;
 import com.zeroone.star.project.dto.j4.student.FollowUpDTO;
+import com.zeroone.star.project.dto.j4.student.StudentDTO;
 import com.zeroone.star.project.query.j4.student.FollowUpQuery;
+import com.zeroone.star.project.query.j4.student.StudentQuery;
 import com.zeroone.star.project.vo.JsonVO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface StudentApis {
+
+    /**
+     * 修改顾问
+     * @param studentDTO
+     * @return
+     */
+    JsonVO<String> modifyConsultant(StudentDTO studentDTO);
+
+    /**
+     * 导入在线学员
+     */
+    JsonVO<String> importOnlineStudents(MultipartFile file);
+
+    /**
+     * 导出在线学员
+     */
+    ResponseEntity<byte[]> exportOnlineStudents();
+
 }
