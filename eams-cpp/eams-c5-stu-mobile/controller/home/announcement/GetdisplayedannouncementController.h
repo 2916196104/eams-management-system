2c0e65021647023b93e934a624d32894efa130b6
a1525 <a1525@pandaafter.com>
2026-03-18 21:22:36 +0800

    控制器迁移(为合并做准备)

--- eams-cpp/eams-c5-stu-mobile/controller/home/announcement/GetdisplayedannouncementController.h
+++ eams-cpp/eams-c5-stu-mobile/controller/home/announcement/GetdisplayedannouncementController.h
@@ -1,1 +1,10 @@
-#pragma once
+#include"stdafx.h"
+#include"GetdisplayedannouncementController.h"
+StringJsonVO::Wrapper GetdisplayedannouncementController::executequerygetdisplayedannouncement(const PageQuery::Wrapper& query)
+{
+    //创建一个响应结果
+    auto vo = StringJsonVO::createShared();
+    vo->success("success");
+    return vo;
+
+}
