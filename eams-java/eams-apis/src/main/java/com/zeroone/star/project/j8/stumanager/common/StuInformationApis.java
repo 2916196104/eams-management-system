b6f49d2028e2b02307b7bf28342bef01d1281aaf
liuchengcheng <liuchengch_2023@qq.com>
2026-03-18 20:19:17 +0800

    修改了StuInformationApis、SwaggerConfig、ProspectiveStuController和RegistryRecordsController

--- eams-java/eams-apis/src/main/java/com/zeroone/star/project/j8/stumanager/common/StuInformationApis.java
+++ eams-java/eams-apis/src/main/java/com/zeroone/star/project/j8/stumanager/common/StuInformationApis.java
@@ -12,20 +12,14 @@ import com.zeroone.star.project.vo.j8.stumanager.StudentListVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@Api(tags = "学员管理")
 /**
  * 学员信息
  */
 public interface StuInformationApis {
 
-    @ApiOperation("保存学员")
     JsonVO<SaveStuDTO> saveStudent(SaveStuDTO stuSaveDTO);
 
-    @ApiOperation("学员阶段设置")
-
     JsonVO<SaveStuAddDTO> setStudentStage(SaveStuAddDTO saveStuAddDTO);
-
-    @ApiOperation("学员报名课程")
 
     JsonVO<StuSignCourseDTO> enrollCourse(StuSignCourseDTO stuSignCourseDTO);
 
