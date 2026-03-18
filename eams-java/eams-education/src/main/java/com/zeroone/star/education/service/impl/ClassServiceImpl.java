68c3d210336cb4c4fee88d413c15a8aed9c0d259
taohu5564 <taohu5564@gmail.com>
2026-03-18 18:58:02 +0800

    班级类的service层创建创建，包含根据班级名称查询班级的下拉接口的实现

---
+++ eams-java/eams-education/src/main/java/com/zeroone/star/education/service/impl/ClassServiceImpl.java
@@ -0,0 +1,28 @@
+package com.zeroone.star.education.service.impl;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.zeroone.star.education.mapper.ClassMapper;
+import com.zeroone.star.education.service.ClassService;
+import com.zeroone.star.project.query.j5.schedule.ClassByNameQuery;
+import com.zeroone.star.project.vo.j5.schedule.ClassOptionVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClassServiceImpl extends ServiceImpl<ClassMapper, Class> implements ClassService {
+    @Autowired
+    private ClassMapper classMapper;
+    /*
+    * 根据班级名查询班级信息，用于下拉菜单
+    * */
+    @Override
+    public Page<ClassOptionVO> listByClassName(ClassByNameQuery query) {
+        // sbl，忘记分页查询要创建分页查询对象了
+        // 1、创建分页查询对象
+        Page<ClassOptionVO> page = new Page<>(query.getPageIndex(), query.getPageSize());
+
+        // 2、因为是多表联查，所以需要用xml获取数据
+        return classMapper.getByClassName(page,query);
+    }
+}
