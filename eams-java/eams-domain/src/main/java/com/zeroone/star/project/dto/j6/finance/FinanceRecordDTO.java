8d1075795d5a71af15e5485236342f1d1373c2e1
chuming <3548473209@qq.com>
2026-03-18 18:14:52 +0800

    更正领域模型

--- eams-java/eams-domain/src/main/java/com/zeroone/star/project/dto/j6/finance/FinanceRecordDTO.java
+++ eams-java/eams-domain/src/main/java/com/zeroone/star/project/dto/j6/finance/FinanceRecordDTO.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 /**
  * 财务记录表
@@ -13,42 +14,30 @@ public class FinanceRecordDTO {
     @ApiModelProperty(value = "财务记录id",example = "123456789")
     private Long id;
 
+    @ApiModelProperty(value = "款项类型",required = true,example = "0")
+    private Integer itemType;
+
     @ApiModelProperty(value = "标题",required = true,example = "样例标题")
     private String title;
 
     @ApiModelProperty(value = "金额,退款为负数",required = true,example = "10000")
     private BigDecimal amount;
 
-    @ApiModelProperty(value = "收入项类型",required = true,example = "0")
-    private Integer itemType;
-
-    @ApiModelProperty(value = "付款人id",required = true,example = "1")
-    private Long payer;
-
-    @ApiModelProperty(value = "学生姓名",required = true,example = "张三")
+    @ApiModelProperty(value = "学员",required = true,example = "张三")
     private String studentName;
 
-    @ApiModelProperty(value = "分校名称",required = true,example = "学校总部")
-    private String schoolName;
-
-    @ApiModelProperty(value = "学习阶段",required = true,example = "1")
-    private Integer stage;
-
-    @ApiModelProperty(value = "经办人id",required = true,example = "1145")
-    private Long operator;
-
-    @ApiModelProperty(value = "经办人姓名",required = true,example = "李四")
+    @ApiModelProperty(value = "经手人",required = true,example = "李四")
     private String handlerName;
 
-    @ApiModelProperty(value = "员工职位",required = true,example = "教职工")
-    private String position;
-
-    @ApiModelProperty(value = "款项备注")
+    @ApiModelProperty(value = "备注说明",example = "这是一个备注说明")
     private String remark;
 
-    @ApiModelProperty(value = "审核确认状态,0：审核中 1：已审核 2：已拒绝",required = true,example = "0")
+    @ApiModelProperty(value = "提交时间",example = "2026-03-15")
+    private LocalDateTime submitTime;
+
+    @ApiModelProperty(value = "认款状态,0：审核中 1：已审核 2：已拒绝",required = true,example = "0")
     private Integer verifyState;
 
-    @ApiModelProperty(value = "审核备注")
+    @ApiModelProperty(value = "认款信息",example = "这是一个认款信息")
     private String verifyRemark;
 }
