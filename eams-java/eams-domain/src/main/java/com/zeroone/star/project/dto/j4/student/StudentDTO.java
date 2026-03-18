e4bf9db9b2644415790053ea6f7cdd04d059ea36
Kiana2233 <1661624270@qq.com>
2026-03-18 10:13:51 +0800

    新加StudentDTO内容

--- eams-java/eams-domain/src/main/java/com/zeroone/star/project/dto/j4/student/StudentDTO.java
+++ eams-java/eams-domain/src/main/java/com/zeroone/star/project/dto/j4/student/StudentDTO.java
@@ -1,4 +1,65 @@
 package com.zeroone.star.project.dto.j4.student;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 //StudentDTO：包含学员基础信息及扩展属性。
+@Data
+@ApiModel("在学学员传输对象")
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentDTO {
+
+    @ApiModelProperty(value = "学员id", example = "1")
+    private Long id;
+
+    @ApiModelProperty(value = "顾问")
+    private Long counselor;
+
+    @ApiModelProperty(value = "手机号")
+    private String mobile;
+
+    @ApiModelProperty(value = "所属校区")
+    private Long schoolId;
+
+    @ApiModelProperty(value = "家长姓名")
+    private String uerName;
+
+    @ApiModelProperty(value = "亲属关系")
+    private Integer familyRel;
+
+    @ApiModelProperty(value = "学员姓名")
+    private String name;
+
+    @ApiModelProperty(value = "性别")
+    private Integer gender;
+
+    @ApiModelProperty(value = "入学时间")
+    private LocalDateTime admissionTime;
+
+    @ApiModelProperty(value = "年级ID")
+    private Integer gradeId;
+
+    @ApiModelProperty(value = "出生年月")
+    private LocalDate birthday;
+
+    @ApiModelProperty(value = "身份证号")
+    private String idCard;
+
+    @ApiModelProperty(value = "备注")
+    private String remark;
+
+    @ApiModelProperty(value = "来源")
+    private String source;
+
+    @ApiModelProperty(value = "学员阶段状态")
+    private Integer stage;
+
+
 }
