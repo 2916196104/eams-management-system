3d206ac3b7b5a8b98aca0b22b373c96a81df91ec
火山 <13215761+huo-shan@user.noreply.gitee.com>
2026-03-18 11:49:43 +0800

    合并【softmaple,枫倾】分支，【数据字典】三接口领域模型：获取字典列表，保存字典，删除字典（支持批量）。

---
+++ eams-java/eams-apis/src/main/java/com/zeroone/star/project/j2/sys/DatadictApis.java
@@ -0,0 +1,39 @@
+package com.zeroone.star.project.j2.sys;
+
+import com.zeroone.star.project.dto.PageDTO;
+import com.zeroone.star.project.dto.j2.sys.DatadictDTO;
+import com.zeroone.star.project.query.PageQuery;
+import com.zeroone.star.project.vo.JsonVO;
+import com.zeroone.star.project.vo.j2.sys.DatadictVO;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 描述：数据字典API接口定义
+ * </p>
+ * @author softmaple
+ * @version 1.0.0
+ */
+public interface DatadictApis {
+    /**
+     * 根据id获取数据字典
+     */
+    JsonVO<DatadictVO> getDatadictById(Integer id);
+    /**
+     * 获取数据字典列表
+     */
+    JsonVO<PageDTO<DatadictVO>> listDatadict(PageQuery query);
+    /**
+     * 增加数据字典
+     */
+    JsonVO<String> addDatadict(DatadictDTO dto);
+    /**
+     * 修改数据字典
+     */
+    JsonVO<String> updateDatadict(DatadictDTO dto);
+    /**
+     * 删除数据字典
+     */
+    JsonVO<String> deleteDatadict(List<Integer> ids);
+}
---
+++ eams-java/eams-j2-sys/src/main/java/com/zeroone/star/sys/controller/DatadictController.java
@@ -0,0 +1,64 @@
+package com.zeroone.star.sys.controller;
+
+import cn.hutool.db.Page;
+import com.zeroone.star.project.dto.PageDTO;
+import com.zeroone.star.project.dto.j2.sys.DatadictDTO;
+import com.zeroone.star.project.j2.sys.DatadictApis;
+import com.zeroone.star.project.query.PageQuery;
+import com.zeroone.star.project.vo.JsonVO;
+import com.zeroone.star.project.vo.j2.sys.DatadictVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 描述：数据字典控制器类
+ * </p>
+ * @author softmaple
+ * @version 1.0.0
+ */
+@RestController
+@RequestMapping("/sys/datadict")
+@Api(tags = "数据字典管理")
+@Validated
+public class DatadictController implements DatadictApis {
+
+    @ApiOperation(value = "根据ID获取数据字典", notes = "根据ID获取数据字典")
+    @GetMapping("/{id}")
+    @Override
+    public JsonVO<DatadictVO> getDatadictById(@PathVariable("id") Integer id) {
+        return null;
+    }
+
+    @GetMapping
+    @ApiOperation(value = "分页查询数据字典列表", notes = "分页查询数据字典列表")
+    @Override
+    public JsonVO<PageDTO<DatadictVO>> listDatadict(@Validated @RequestBody PageQuery query) {
+        return null;
+    }
+
+    @PostMapping
+    @ApiOperation(value = "保存数据字典", notes = "保存数据字典")
+    @Override
+    public JsonVO<String> addDatadict(@Validated @RequestBody DatadictDTO dto) {
+        return null;
+    }
+
+    @PutMapping
+    @ApiOperation(value = "修改数据字典", notes = "修改数据字典")
+    @Override
+    public JsonVO<String> updateDatadict(@Validated @RequestBody DatadictDTO dto) {
+        return null;
+    }
+
+    @DeleteMapping
+    @ApiOperation(value = "删除数据字典", notes = "删除数据字典")
+    @Override
+    public JsonVO<String> deleteDatadict(@RequestBody List<Integer> ids) {
+        return null;
+    }
+}
