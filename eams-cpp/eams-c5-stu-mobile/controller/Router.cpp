b077fdde3d66babbce5da5f671a212f956981865
jinlongyueyu <2922518640@qq.com>
2026-03-19 08:50:21 +0800

    完成了点评记录接口的邻域模型定义以及具体实现

--- eams-cpp/eams-c5-stu-mobile/controller/Router.cpp
+++ eams-cpp/eams-c5-stu-mobile/controller/Router.cpp
@@ -19,6 +19,7 @@
 #include "stdafx.h"
 #include "Router.h"
 #include "ApiHelper.h"
+#include "../controller/home/LessonComment/LessonCommentController.h"
 
 // 如果定义了关闭Swagger文档宏
 #ifdef CLOSE_SWAGGER_DOC
@@ -40,5 +41,5 @@ Router::Router(Endpoints* docEndpoints, HttpRouter* router)
 void Router::initRouter()
 {
 	//#TIP :系统扩展路由定义，写在这个后面
-	
+	ROUTER_SIMPLE_BIND(LessonCommentController);
 }
