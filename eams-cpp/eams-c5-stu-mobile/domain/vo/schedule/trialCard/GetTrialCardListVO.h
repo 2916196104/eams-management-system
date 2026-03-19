9b9264938e3c39371cb1b8151a3562d43efcf21f
monster <262661897@qq.com>
2026-03-19 12:53:03 +0800

    [add] 获取试听卡列表的控制器

---
+++ eams-cpp/eams-c5-stu-mobile/domain/vo/schedule/trialCard/GetTrialCardListVO.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef _GETTRIALCARDLISTDTO_
+#define _GETTRIALCARDLISTDTO_
+
+#include "../../../GlobalInclude.h"
+#include "domain/dto/schedule/trialCard/GetTrialCardListDTO.h"
+
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+class GetTrialCardListJsonVO : public JsonVO<GetTrialCardListDTO::Wrapper>
+{
+	DTO_INIT(GetTrialCardListJsonVO, JsonVO<GetTrialCardListDTO::Wrapper>)
+};
+
+class GetTrialCardListPageJsonVO : JsonVO<GetTrialCardLIstPageDTO::Wrapper>
+{
+	DTO_INIT(GetTrialCardListPageJsonVO, JsonVO<GetTrialCardLIstPageDTO::Wrapper>)
+};
+
+#include OATPP_CODEGEN_END(DTO)
+
+#endif // !_GETTRIALCARDLISTDTO_
