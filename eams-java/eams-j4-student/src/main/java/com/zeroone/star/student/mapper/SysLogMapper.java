cabdee7fcd671a98608c9ceb5d6574c452a56c86
Kiana2233 <1661624270@qq.com>
2026-03-19 21:14:45 +0800

    修改在学学员顾问

--- eams-java/eams-j4-student/src/main/java/com/zeroone/star/student/mapper/SysLogMapper.java
+++ eams-java/eams-j4-student/src/main/java/com/zeroone/star/student/mapper/SysLogMapper.java
@@ -2,6 +2,8 @@ package com.zeroone.star.student.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zeroone.star.student.domain.po.SysLog;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface SysLogMapper extends BaseMapper<SysLog> {
 }
