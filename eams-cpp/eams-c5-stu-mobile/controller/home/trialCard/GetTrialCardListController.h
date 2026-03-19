6b39a296902926433bc0198b84895df0a29f4281
monster <262661897@qq.com>
2026-03-19 22:43:32 +0800

    [feat] 为了合并分支做了些路径上的调整

--- eams-cpp/eams-c5-stu-mobile/controller/home/trialCard/GetTrialCardListController.h
+++ eams-cpp/eams-c5-stu-mobile/controller/home/trialCard/GetTrialCardListController.h
@@ -13,11 +13,11 @@ class GetTrialCardListController : public oatpp::web::server::api::ApiController
 public:
 	// 定义ID查询接口描述
 	API_DEF_ENDPOINT_INFO_AUTH(
-		ZH_WORDS_GETTER("home.trialCard.getTrialCardList.info"), getTrialCardList, GetTrialCardListPageJsonVO::Wrapper, "Schedule",
-		API_DEF_ADD_QUERY_PARAMS(String, "id", ZH_WORDS_GETTER("student.id"); , "d934050a8bb373e8f8eed0bf7507ec17", true);
+		ZH_WORDS_GETTER("c5.home.trialCard.getTrialCardList.info"), getTrialCardList, GetTrialCardListPageJsonVO::Wrapper, "Schedule",
+		API_DEF_ADD_QUERY_PARAMS(String, "id", ZH_WORDS_GETTER("c5.student.id"); , "d934050a8bb373e8f8eed0bf7507ec17", true);
 	);
 	// 定义ID查询接口处理
-	API_HANDLER_ENDPOINT_AUTH(API_M_GET, "/app/sCenter/home/trialCard/GetTrialCardList", 
+	API_HANDLER_ENDPOINT_AUTH(API_M_GET, "/c5/home/trialCard/GetTrialCardList", 
 		getTrialCardList, QUERY(String, id), executeGetCardById(id));
 private:
 	GetTrialCardListPageJsonVO::Wrapper executeGetCardById(String id);
--- eams-cpp/eams-c5-stu-mobile/domain/dto/home/trialCard/GetTrialCardListDTO.h
+++ eams-cpp/eams-c5-stu-mobile/domain/dto/home/trialCard/GetTrialCardListDTO.h
@@ -12,31 +12,31 @@ class GetTrialCardListAddDTO : public oatpp::DTO
 	DTO_FIELD(String, name); // 试用卡名称
 	DTO_FIELD_INFO(name)
 	{
-		info->description = ZH_WORDS_GETTER("home.trialCard.name");
+		info->description = ZH_WORDS_GETTER("c5.home.trialCard.name");
 	}
 		
 	DTO_FIELD(String, courseName); // 试用卡课程名称
 	DTO_FIELD_INFO(courseName)
 	{
-		info->description = ZH_WORDS_GETTER("home.trialCard.courseName");
+		info->description = ZH_WORDS_GETTER("c5.home.trialCard.courseName");
 	}
 
 	DTO_FIELD(Int32, lessonCount); // 试用卡课时
 	DTO_FIELD_INFO(lessonCount)
 	{
-		info->description = ZH_WORDS_GETTER("home.trialCard.lessonCount");
+		info->description = ZH_WORDS_GETTER("c5.home.trialCard.lessonCount");
 	}
 
 	DTO_FIELD(Int32, remainingQuantity); // 试用卡剩余数量
 	DTO_FIELD_INFO(remainingQuantity)
 	{
-		info->description = ZH_WORDS_GETTER("home.trialCard.remainingQuantity");
+		info->description = ZH_WORDS_GETTER("c5.home.trialCard.remainingQuantity");
 	}
 
 	DTO_FIELD(String, endData); // 试用卡截止时间
 	DTO_FIELD_INFO(endData)
 	{
-		info->description = ZH_WORDS_GETTER("home.trialCard.endData");
+		info->description = ZH_WORDS_GETTER("c5.home.trialCard.endData");
 	}
 };
 
@@ -47,7 +47,7 @@ class GetTrialCardListDTO : public GetTrialCardListAddDTO
 	DTO_FIELD(String, id); // 试用卡id
 	DTO_FIELD_INFO(id)
 	{
-		info->description = ZH_WORDS_GETTER("student.id");
+		info->description = ZH_WORDS_GETTER("c5.student.id");
 	}
 };
 
