de8762ce02f73816a230a5b751843ef81ae42e9d
changan <2022635252@qq.com>
2026-03-20 17:04:01 +0800

    新增领域模型以及需求接口定义，完成进度100%

--- eams-cpp/eams-c2-org/controller/staff/StaffController.h
+++ eams-cpp/eams-c2-org/controller/staff/StaffController.h
@@ -6,6 +6,9 @@
 #include "domain/vo/BaseJsonVO.h"
 #include "ApiHelper.h"
 #include "ServerInfo.h"
+#include "Macros.h"
+#include "domain/query/PageQuery.h"
+#include "../../domain/dto/staff/StaffDTO.h"
 
 // 1 定义API控制器使用宏
 #include OATPP_CODEGEN_BEGIN(ApiController)
@@ -20,8 +23,58 @@ class StaffController : public oatpp::web::server::api::ApiController // 1 继�
   API_ACCESS_DECLARE(StaffController);
   // 3 定义接口
 public:
+	// 定义接口:获取员工列表
+	// 接口描述
+	ENDPOINT_INFO(getEmpList) {
+		// 接口描述
+		info->summary = ZH_WORDS_GETTER("getEmpList.controller.summary");
+
+		// 支持授权(文档描述时显示锁)
+		// 同时要向ENDPOINT加入参数五
+		API_DEF_ADD_AUTH();
+
+		// 定义响应参数格式
+		API_DEF_ADD_RSP_JSON(StringJsonVO::Wrapper);// 文档中会展示响应参数格式
+
+		// 定义请求参数格式
+		// 分页参数："查询页码"、"每页的条数"
+		API_DEF_ADD_PAGE_PARAMS();// 这个宏用于定义"分页相关"的查询参数文档(会向文档中加入"查询页码"、"每页的条数"参数)
+		// 其他参数："姓名/手机号"、"状态"
+		API_DEF_ADD_QUERY_PARAMS(String, "name", ZH_WORDS_GETTER("getEmpList.controller.empName"), ZH_WORDS_GETTER("getEmpList.controller.zhangsan"), false);
+		API_DEF_ADD_QUERY_PARAMS(String, "phone", ZH_WORDS_GETTER("getEmpList.controller.phone"), "13800138000", false);
+		API_DEF_ADD_QUERY_PARAMS(Int32, "status", ZH_WORDS_GETTER("getEmpList.controller.status"), 1, false);
+	}
+	
+	// 接口定义(路由注册)
+	// 需要的查询参数有多个，所以参数四用QUERIES(变量类型, 变量名)
+	ENDPOINT(API_M_GET, "/org/staff/getEmpList", getEmpList, QUERIES(QueryParams, params), API_HANDLER_AUTH_PARAME) {
+		// 解析报文参数:params-->PageQuery类型的query变量
+		API_HANDLER_QUERY_PARAM(query, StaffPageQuery, params); // PageQuery类是专为分页查询的数据对象,详情看其定义
+
+		// 响应业务处理函数的处理结果(executeGetEmpList会返回VO，之后传参)
+		API_HANDLER_RESP_VO(executeGetEmpList(query));
+
+	}
+
+
+
+	// 定义接口:保存员工
+
+	// 3.1 定义新增接口描述
+	API_DEF_ENDPOINT_INFO_AUTH(ZH_WORDS_GETTER("saveEmp.controller.summary"), saveEmp, StringJsonVO::Wrapper, API_TAG);
+
+
+	// 3.2 定义新增接口处理
+	API_HANDLER_ENDPOINT_AUTH(API_M_POST, "/org/staff/saveEmp", saveEmp, BODY_DTO(StaffSaveDTO::Wrapper, dto), executeSaveEmp(dto));
+	
 
 private: // 定义接口执行函数
+	// 获取员工列表
+	StringJsonVO::Wrapper executeGetEmpList(const StaffPageQuery::Wrapper& query);
+
+	// 保存员工(新增员工+更新员工)
+	StringJsonVO::Wrapper executeSaveEmp(const StaffSaveDTO::Wrapper& dto);
+
 };
 
 #undef API_TAG
--- eams-cpp/eams-c2-org/domain/dto/staff/StaffDTO.h
+++ eams-cpp/eams-c2-org/domain/dto/staff/StaffDTO.h
@@ -11,8 +11,104 @@
 	员工数据传输对象
  */
 class StaffDTO : public oatpp::DTO {
-  DTO_INIT(StaffDTO, DTO);
+	DTO_INIT(StaffDTO, DTO);
+
+
+
+
 };
+
+
+/**
+ * 保存员工参数对象
+ */
+class StaffSaveDTO : public StaffDTO
+{
+	DTO_INIT(StaffSaveDTO, StaffDTO);
+
+	//// 添加成员变量:id
+	//DTO_FIELD(UInt64, id);
+	//DTO_FIELD_INFO(id) {
+	//	info->description = ZH_WORDS_GETTER("saveEmp.dto.id");
+	//}
+	API_DTO_FIELD_REQUIRE(UInt64, id, ZH_WORDS_GETTER("saveEmp.dto.id"), false);         // 可选，新增不用传
+
+
+	// 姓名
+	API_DTO_FIELD_REQUIRE(String, name, ZH_WORDS_GETTER("saveEmp.dto.name"), true);
+	// 电话/登录账号
+	API_DTO_FIELD_REQUIRE(String, mobile, ZH_WORDS_GETTER("saveEmp.dto.mobile"), true);
+	// 所属机构
+	API_DTO_FIELD_REQUIRE(Int64, orgId, ZH_WORDS_GETTER("saveEmp.dto.orgId"), true);
+	// 所属职位
+	API_DTO_FIELD_REQUIRE(Int64, positionId, ZH_WORDS_GETTER("saveEmp.dto.positionId"), true);
+	// 性别
+	API_DTO_FIELD_REQUIRE(Int32, gender, ZH_WORDS_GETTER("saveEmp.dto.gender"), false);
+	// 学历
+	API_DTO_FIELD_REQUIRE(Int32, degree, ZH_WORDS_GETTER("saveEmp.dto.degree"), false);
+	// 生日
+	API_DTO_FIELD_REQUIRE(String, birthday, ZH_WORDS_GETTER("saveEmp.dto.birthday"), false);
+	// 毕业学校
+	API_DTO_FIELD_REQUIRE(String, school, ZH_WORDS_GETTER("saveEmp.dto.school"), false);
+	// 入职日期
+	API_DTO_FIELD_REQUIRE(String, hireDate, ZH_WORDS_GETTER("saveEmp.dto.hireDate"), false);
+	// 上课费
+	API_DTO_FIELD_REQUIRE(Float64, classFee, ZH_WORDS_GETTER("saveEmp.dto.classFee"), false);
+	// 助教费
+	API_DTO_FIELD_REQUIRE(Float64, assistantFee, ZH_WORDS_GETTER("saveEmp.dto.assistantFee"), false);
+	// 简介
+	API_DTO_FIELD_REQUIRE(String, intro, ZH_WORDS_GETTER("saveEmp.dto.intro"), false);
+	// 是否是内部人员
+	API_DTO_FIELD_REQUIRE(Boolean, isInner, ZH_WORDS_GETTER("saveEmp.dto.isInner"), false);
+	// 是否是管理者
+	API_DTO_FIELD_REQUIRE(Boolean, isManager, ZH_WORDS_GETTER("saveEmp.dto.isManager"), false);
+
+public:
+
+};
+
+
+
+
+
+
+/**
+ * 员工列表分页传输对象
+ */
+class StaffPageDTO : public PageDTO<StaffDTO::Wrapper>
+{
+	DTO_INIT(StaffPageDTO, PageDTO<StaffDTO::Wrapper>);
+
+
+};
+
+
+/**
+ * 员工列表查询参数对象
+ */
+class StaffPageQuery : public oatpp::DTO {
+	DTO_INIT(StaffPageQuery, DTO);
+
+
+	
+	//API_DTO_FIELD_REQUIRE(UInt64, pageIndex, ZH_WORDS_GETTER("getEmpList.dto.pageIndex"), true);
+	API_DTO_FIELD(UInt64, pageIndex, ZH_WORDS_GETTER("getEmpList.dto.pageIndex"), true, 1);
+
+	
+	//API_DTO_FIELD_REQUIRE(UInt64, pageSize, ZH_WORDS_GETTER("getEmpList.dto.pageSize"), true);
+	API_DTO_FIELD(UInt64, pageSize, ZH_WORDS_GETTER("getEmpList.dto.pageSize"), true, 10);
+
+
+	API_DTO_FIELD_REQUIRE(String, name, ZH_WORDS_GETTER("getEmpList.dto.name"), false);
+
+	
+	API_DTO_FIELD_REQUIRE(String, phone, ZH_WORDS_GETTER("getEmpList.dto.phone"), false);
+	
+	
+	API_DTO_FIELD_REQUIRE(Int32, status, ZH_WORDS_GETTER("getEmpList.dto.status"), false);
+};
+
+
 
 #include OATPP_CODEGEN_END(DTO)
 #endif // _STAFFDTO_H_
