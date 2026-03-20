6fa36af3f0b869228a7f9a051ca790eb7409071b
wanglongshen <19532295223@163.com>
2026-03-20 16:16:55 +0800

    新增头像修改接口

--- eams-cpp/eams-c2-org/controller/staff/StaffController.cpp
+++ eams-cpp/eams-c2-org/controller/staff/StaffController.cpp
@@ -1,2 +1,12 @@
 #include "stdafx.h"
 #include "StaffController.h"
+
+
+
+// 头像修改逻辑实现（和executeQueryTest格式一致）
+StringJsonVO::Wrapper StaffController::executeModifyAvatar(const AvatarUploadDTO::Wrapper& dto) {
+	// 创建响应结果
+	auto vo = StringJsonVO::createShared();
+	vo->success(ZH_WORDS_GETTER("modifyAvatar.resp"));
+	return vo;
+}
--- eams-cpp/eams-c2-org/controller/staff/StaffController.h
+++ eams-cpp/eams-c2-org/controller/staff/StaffController.h
@@ -6,6 +6,10 @@
 #include "domain/vo/BaseJsonVO.h"
 #include "ApiHelper.h"
 #include "ServerInfo.h"
+#include "Macros.h"
+#include"domain/query/PageQuery.h"
+#include "domain/dto/staff/StaffDTO.h" 
+
 
 // 1 定义API控制器使用宏
 #include OATPP_CODEGEN_BEGIN(ApiController)
@@ -20,10 +24,28 @@ class StaffController : public oatpp::web::server::api::ApiController // 1 继�
   API_ACCESS_DECLARE(StaffController);
   // 3 定义接口
 public:
+	
 
+	// 定义修改头像接口
+	ENDPOINT_INFO(modifyAvatar) {
+		info->summary = ZH_WORDS_GETTER("modifyAvatar.summary");
+		// 支持授权
+		API_DEF_ADD_AUTH();
+		// 定义响应参数格式
+		API_DEF_ADD_RSP_JSON(StringJsonVO::Wrapper);
+		//声明接收文件表单，定义参数请求格式
+		API_DEF_ADD_FILE_FORM(AvatarUploadDTO::Wrapper);
+
+	}
+	ENDPOINT(API_M_POST, "/org/employee/modifyAvatar", modifyAvatar, BODY_DTO(AvatarUploadDTO::Wrapper,dto), API_HANDLER_AUTH_PARAME) {
+		// 直接调用处理函数
+		API_HANDLER_RESP_VO(executeModifyAvatar(dto));
+		//return createResponse(Status::CODE_200, ZH_WORDS_GETTER("modifyAvatar.resp"));
+	}
 private: // 定义接口执行函数
+	StringJsonVO::Wrapper executeModifyAvatar(const AvatarUploadDTO::Wrapper& dto);
 };
 
 #undef API_TAG
 #include OATPP_CODEGEN_END(ApiController) //<- End Codegen
-#endif // !_POSITION_CONTROLLER_
+#endif // !_STAFF_CONTROLLER_
