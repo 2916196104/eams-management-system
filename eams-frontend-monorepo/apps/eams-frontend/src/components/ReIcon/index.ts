911ad587978d8b2655fd3a6095ade4f8c27cafca
ruan-cat <1219043956@qq.com>
2026-03-20 22:34:16 +0800

    ✨ feat(reicon): 新增后台 iconify 图标基础设施
    为后台项目补充 ReIcon 组件目录，提供离线图标、在线图标、渲染 hook、
    类型定义和组件说明文档，为后续页面和菜单接入 iconify 做统一封装。

---
+++ eams-frontend-monorepo/apps/eams-frontend/src/components/ReIcon/index.ts
@@ -0,0 +1,7 @@
+import IconifyIconOffline from './src/iconifyIconOffline'
+import IconifyIconOnline from './src/iconifyIconOnline'
+import { pickIconRenderer, useRenderIcon } from './src/hooks'
+import { registerOfflineIcons, resolveOfflineIcon } from './src/offlineIcon'
+export type { ReIconOption, ReIconProps } from './type'
+
+export { IconifyIconOffline, IconifyIconOnline, pickIconRenderer, registerOfflineIcons, resolveOfflineIcon, useRenderIcon }
---
+++ eams-frontend-monorepo/apps/eams-frontend/src/components/ReIcon/src/offlineIcon.ts
@@ -0,0 +1,107 @@
+import type { App, Component } from 'vue'
+import { markRaw } from 'vue'
+import EpArrowDownBold from '~icons/ep/arrow-down-bold'
+import EpArrowUpBold from '~icons/ep/arrow-up-bold'
+import EpBack from '~icons/ep/back'
+import EpCheck from '~icons/ep/check'
+import EpClose from '~icons/ep/close'
+import EpCloseBold from '~icons/ep/close-bold'
+import EpDelete from '~icons/ep/delete'
+import EpDownload from '~icons/ep/download'
+import EpEdit from '~icons/ep/edit'
+import EpExpand from '~icons/ep/expand'
+import EpFinished from '~icons/ep/finished'
+import EpFold from '~icons/ep/fold'
+import EpFullScreen from '~icons/ep/full-screen'
+import EpHomeFilled from '~icons/ep/home-filled'
+import EpMenu from '~icons/ep/menu'
+import EpMessage from '~icons/ep/message'
+import EpMinus from '~icons/ep/minus'
+import EpMore from '~icons/ep/more'
+import EpMoreFilled from '~icons/ep/more-filled'
+import EpOperation from '~icons/ep/operation'
+import EpPlus from '~icons/ep/plus'
+import EpRefreshRight from '~icons/ep/refresh-right'
+import EpSearch from '~icons/ep/search'
+import EpSetting from '~icons/ep/setting'
+import EpTickets from '~icons/ep/tickets'
+import EpUploadFilled from '~icons/ep/upload-filled'
+import EpUser from '~icons/ep/user'
+
+const offlineIcons = {
+	'ep/arrow-down-bold': markRaw(EpArrowDownBold),
+	'ep/arrow-up-bold': markRaw(EpArrowUpBold),
+	'ep/back': markRaw(EpBack),
+	'ep/check': markRaw(EpCheck),
+	'ep/close': markRaw(EpClose),
+	'ep/close-bold': markRaw(EpCloseBold),
+	'ep/delete': markRaw(EpDelete),
+	'ep/download': markRaw(EpDownload),
+	'ep/edit': markRaw(EpEdit),
+	'ep/expand': markRaw(EpExpand),
+	'ep/finished': markRaw(EpFinished),
+	'ep/fold': markRaw(EpFold),
+	'ep/full-screen': markRaw(EpFullScreen),
+	'ep/home-filled': markRaw(EpHomeFilled),
+	'ep/menu': markRaw(EpMenu),
+	'ep/message': markRaw(EpMessage),
+	'ep/minus': markRaw(EpMinus),
+	'ep/more': markRaw(EpMore),
+	'ep/more-filled': markRaw(EpMoreFilled),
+	'ep/operation': markRaw(EpOperation),
+	'ep/plus': markRaw(EpPlus),
+	'ep/refresh-right': markRaw(EpRefreshRight),
+	'ep/search': markRaw(EpSearch),
+	'ep/setting': markRaw(EpSetting),
+	'ep/tickets': markRaw(EpTickets),
+	'ep/upload-filled': markRaw(EpUploadFilled),
+	'ep/user': markRaw(EpUser)
+} satisfies Record<string, Component>
+
+const aliasIcons = {
+	IconArrowDownBold: offlineIcons['ep/arrow-down-bold'],
+	IconArrowUpBold: offlineIcons['ep/arrow-up-bold'],
+	IconBack: offlineIcons['ep/back'],
+	IconCheck: offlineIcons['ep/check'],
+	IconClose: offlineIcons['ep/close'],
+	IconCloseBlod: offlineIcons['ep/close-bold'],
+	IconDelete: offlineIcons['ep/delete'],
+	IconDownload: offlineIcons['ep/download'],
+	IconEdit: offlineIcons['ep/edit'],
+	IconExpand: offlineIcons['ep/expand'],
+	IconFinished: offlineIcons['ep/finished'],
+	IconFold: offlineIcons['ep/fold'],
+	IconFullScreen: offlineIcons['ep/full-screen'],
+	IconHomeFilled: offlineIcons['ep/home-filled'],
+	IconMenu: offlineIcons['ep/menu'],
+	IconMessage: offlineIcons['ep/message'],
+	IconMinus: offlineIcons['ep/minus'],
+	IconMore: offlineIcons['ep/more'],
+	IconMoreFilled: offlineIcons['ep/more-filled'],
+	IconOperation: offlineIcons['ep/operation'],
+	IconPlus: offlineIcons['ep/plus'],
+	IconReset: offlineIcons['ep/refresh-right'],
+	IconSearch: offlineIcons['ep/search'],
+	IconSetting: offlineIcons['ep/setting'],
+	IconTickets: offlineIcons['ep/tickets'],
+	IconUpload: offlineIcons['ep/upload-filled'],
+	IconUser: offlineIcons['ep/user']
+} satisfies Record<string, Component>
+
+function normalizeOfflineIconName(icon: string): string {
+	return icon.replace(/:/g, '/').trim()
+}
+
+export function resolveOfflineIcon(icon: string): Component | undefined {
+	if (icon in aliasIcons) {
+		return aliasIcons[icon as keyof typeof aliasIcons]
+	}
+
+	return offlineIcons[normalizeOfflineIconName(icon) as keyof typeof offlineIcons]
+}
+
+export function registerOfflineIcons(app: App) {
+	for (const [name, component] of Object.entries(aliasIcons)) {
+		app.component(name, component)
+	}
+}
