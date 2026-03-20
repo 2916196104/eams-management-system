d602c7f26083158f320a9edce3d8eafd480c8de1
lingluody <1035907499@qq.com>
2026-03-20 17:58:44 +0800

    修复CommmonDTO.h中出现的编译错误

--- eams-cpp/eams-c6-student/domain/dto/common/CommonDTO.h
+++ eams-cpp/eams-c6-student/domain/dto/common/CommonDTO.h
@@ -55,7 +55,8 @@ class RegistrationPageDTO : public PageDTO<RegistrationDTO::Wrapper> {
 };
 class FileOnlyDTO :public oatpp::DTO {
 	DTO_INIT(FileOnlyDTO, DTO);
-    API_DTO_FIELD_DEFAULT(oatpp::swagger::Binary, file, ZH_WORDS_GETTER("common.field.file.file"),true);
+    //API_DTO_FIELD_DEFAULT(oatpp::swagger::Binary, file, ZH_WORDS_GETTER("common.field.file.file"),true);多出第四个参数导致编译错误
+	API_DTO_FIELD_REQUIRE(oatpp::swagger::Binary, file, ZH_WORDS_GETTER("common.field.file.file"), true);
 };
 #include OATPP_CODEGEN_END(DTO)
 #endif
