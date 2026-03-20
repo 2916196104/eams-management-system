3fba2124bd4861ac07dd9a3153bc337432ed38a2
silvan2077 <18669513992@163.com>
2026-03-20 16:15:09 +0800

    第二次提交

---
+++ eams-java/eams-sample/src/main/java/com/zeroone/star/sample/service/ISampleService.java
@@ -0,0 +1,16 @@
+package com.zeroone.star.sample.service;
+
+import com.zeroone.star.sample.entity.Sample;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 演示示例表 服务类
+ * </p>
+ *
+ * @author silvan
+ * @since 2026-03-12
+ */
+public interface ISampleService extends IService<Sample> {
+
+}
