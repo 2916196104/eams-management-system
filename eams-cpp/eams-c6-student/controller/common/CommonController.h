220f788620bab311b3d233fe23141cf734fd8e2a
yzhi <3470623528@qq.com>
2026-03-20 15:20:20 +0800

    接口（1）

--- eams-cpp/eams-c6-student/controller/common/CommonController.h
+++ eams-cpp/eams-c6-student/controller/common/CommonController.h
@@ -1,1 +1,50 @@
 #pragma once
+#ifndef COMMON_CONTROLLER_H
+#define COMMON_CONTROLLER_H
+#include "ApiHelper.h"
+#include "../../Macros.h"
+#include "ServerInfo.h"
+#include "domain/vo/BaseJsonVO.h"
+#include "domain/vo/common/CommonVO.h"
+#include "domain/dto/common/CommonDTO.h"
+#include "domain/query/common/CommonQuery.h"
+#include "service/common/CommonService.h"
+
+#include OATPP_CODEGEN_BEGIN(ApiController)
+#define API_TAG ZH_WORDS_GETTER("common.api-tag")
+class CommonController : public oatpp::web::server::api::ApiController {
+	API_ACCESS_DECLARE(CommonController);
+public:
+	ENDPOINT_INFO(getCourseCountData) {
+		API_DEF_ADD_AUTH();//添加权限验证
+		API_DEF_ADD_TAG(API_TAG);//将接口添加到分组
+		API_DEF_ADD_TITLE(ZH_WORDS_GETTER("common.interface.get-student-course-count"));//添加接口描述
+		API_DEF_ADD_PATH_PARAMS(String, "id", ZH_WORDS_GETTER("common.field.student.id"), "123", true);//添加参数
+		API_DEF_ADD_RSP_JSON_WRAPPER(CourseCountJsonVO);//添加返回数据包装
+	}
+	ENDPOINT(API_M_GET, "/c3/common/interface/get-student-course-count", getCourseCountData, PATH(String,id), API_HANDLER_AUTH_PARAME) {
+		API_HANDLER_RESP_VO(executeGetCourseCountData(id));
+	}
+
+	ENDPOINT_INFO(SaveStudent) {
+		API_DEF_ADD_AUTH();//添加权限验证
+		API_DEF_ADD_TAG(API_TAG);//将接口添加到分组
+		API_DEF_ADD_TITLE(ZH_WORDS_GETTER("common.interface.save-student"));//添加接口描述
+		API_DEF_ADD_PATH_PARAMS(String, "id", ZH_WORDS_GETTER("common.field.student.id"), "123", true);//添加参数
+		API_DEF_ADD_RSP_JSON_WRAPPER(StringJsonVO);//添加返回数据包装
+	}
+	ENDPOINT(API_M_GET, "/c3/common/interface/save-student", SaveStudent, PATH(String, id), BODY_DTO(SaveStudentDTO::Wrapper, dto), API_HANDLER_AUTH_PARAME) {
+		API_HANDLER_RESP_VO(executeSaveStudent(id));
+	}
+private:
+	CourseCountJsonVO::Wrapper executeGetCourseCountData(const String& id) {
+		return CourseCountJsonVO::createShared();
+	}
+
+	StringJsonVO::Wrapper executeSaveStudent(const String& id) {
+		return StringJsonVO::createShared();
+	}
+};
+#undef API_TAG
+#include OATPP_CODEGEN_END(ApiController) 
+#endif
--- eams-cpp/eams-c6-student/domain/vo/common/CommonVO.h
+++ eams-cpp/eams-c6-student/domain/vo/common/CommonVO.h
@@ -1,1 +1,14 @@
 #pragma once
+#ifndef __COMMON_VO_H__
+#define __COMMON_VO_H__ 
+#include"../../GlobalInclude.h"
+#include"../../dto/common/CommonDTO.h"
+#include "oatpp/core/macro/codegen.hpp"
+#include OATPP_CODEGEN_BEGIN(DTO)
+class CourseCountJsonVO : public JsonVO<CourseCountDTO::Wrapper> {
+public:
+    DTO_INIT(CourseCountJsonVO, JsonVO<CourseCountDTO::Wrapper>)
+};
+
+#include OATPP_CODEGEN_END(DTO)
+#endif
