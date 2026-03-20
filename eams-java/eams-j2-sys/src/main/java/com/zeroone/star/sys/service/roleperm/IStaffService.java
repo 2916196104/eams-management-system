bb862dd256675eb5bc4279e18afe3388f0268ec2
火山 <13215761+huo-shan@user.noreply.gitee.com>
2026-03-20 22:23:30 +0800

    生成staff表的entity，mapper，service

---
+++ eams-java/eams-j2-sys/src/main/java/com/zeroone/star/sys/service/roleperm/IStaffService.java
@@ -0,0 +1,16 @@
+package com.zeroone.star.sys.service.roleperm;
+
+import com.zeroone.star.sys.entity.roleperm.Staff;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 教师员工表 服务类
+ * </p>
+ *
+ * @author isme
+ * @since 2026-03-20
+ */
+public interface IStaffService extends IService<Staff> {
+
+}
