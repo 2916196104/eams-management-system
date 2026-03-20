cc825efc49554c3dd2954fb3a0640ea4aae8f652
yang <151@com>
2026-03-20 12:17:39 +0800

    接口定义完成

--- eams-cpp/eams-c2-me/controller/student/StudentController.cpp
+++ eams-cpp/eams-c2-me/controller/student/StudentController.cpp
@@ -1,2 +1,13 @@
 #include "stdafx.h"
 #include "StudentController.h"
+
+
+
+GradeJsonVO::Wrapper StudentController::executeQueryGrade(const GradeQuery::Wrapper& query) {
+	auto data = GradeDTO::createShared();
+	auto vo = GradeJsonVO::createShared();
+	vo->success(data);
+	return vo;
+}
+
+
