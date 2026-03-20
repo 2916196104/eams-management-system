e0f645d73e6e3fb59a0bdbcb7f9d8a1e60b61b82
silvan2077 <18669513992@163.com>
2026-03-20 17:38:55 +0800

    第三次提交

---
+++ eams-java/eams-j5-courseStudent/src/test/java/com/xuecheng/eamsj5coursestudent/EamsJ5CourseStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuecheng.eamsj5coursestudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EamsJ5CourseStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
