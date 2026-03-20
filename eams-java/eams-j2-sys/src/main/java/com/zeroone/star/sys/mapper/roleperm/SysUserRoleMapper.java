8e956087e4158302571b7bbb4fa36ed347ec692b
火山 <13215761+huo-shan@user.noreply.gitee.com>
2026-03-20 22:08:57 +0800

    生成sys_user_role表的entity，mapper，service

---
+++ eams-java/eams-j2-sys/src/main/java/com/zeroone/star/sys/mapper/roleperm/SysUserRoleMapper.java
@@ -0,0 +1,18 @@
+package com.zeroone.star.sys.mapper.roleperm;
+
+import com.zeroone.star.sys.entity.roleperm.SysUserRole;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 用户角色 Mapper 接口
+ * </p>
+ *
+ * @author isme
+ * @since 2026-03-20
+ */
+@Mapper
+public interface SysUserRoleMapper extends BaseMapper<SysUserRole> {
+
+}
