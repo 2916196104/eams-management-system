7cac66ad9c0d51b181dcc480918ecaec273ce0e2
zjh <1658080402@qq.com>
2026-03-20 17:40:34 +0800

    manageorg模块路径修改1.0

--- eams-cpp/eams-c3-managerorg/controller/BoardManager/BoardManager.h
+++ eams-cpp/eams-c3-managerorg/controller/BoardManager/BoardManager.h
@@ -25,22 +25,22 @@ public:		//定义接口
 	// 定义保存公告接口描述
 	API_DEF_ENDPOINT_INFO_AUTH(ZH_WORDS_GETTER("board.save.summary"), SaveBoard, StringJsonVO::Wrapper, API_TAG);
 	// 定义保存公告接口端点
-	API_HANDLER_ENDPOINT_AUTH(API_M_POST, "/board/add-board", SaveBoard, BODY_DTO(BoardAddDTO::Wrapper, dto), executeAddBoard(dto, authObject->getPayload()));
+	API_HANDLER_ENDPOINT_AUTH(API_M_POST, "org/boardmanager/save-board", SaveBoard, BODY_DTO(BoardAddDTO::Wrapper, dto), executeAddBoard(dto, authObject->getPayload()));
 	
 	// 3.1 定义删除公告（支持批量删除）接口描述
 	API_DEF_ENDPOINT_INFO_AUTH(ZH_WORDS_GETTER("board.delete.summary"), removeBoard, ListJsonVO<String>::Wrapper, API_TAG);
 	// 3.2 定义删除公告（支持批量删除）接口处理
-	API_HANDLER_ENDPOINT_AUTH(API_M_DEL, "/board/remove-board", removeBoard, BODY_DTO(List<String>, ids), execRemoveBoard(ids));
+	API_HANDLER_ENDPOINT_AUTH(API_M_DEL, "org/boardmanager/remove-board", removeBoard, BODY_DTO(List<String>, ids), execRemoveBoard(ids));
 	
 	// 3.1 定义启用公告接口描述
 	API_DEF_ENDPOINT_INFO_AUTH(ZH_WORDS_GETTER("board.start.summary"), startBoard, ListJsonVO<String>::Wrapper, API_TAG);
 	// 3.2 定义启用公告接口处理
-	API_HANDLER_ENDPOINT_AUTH(API_M_PUT, "/board/start-board", startBoard, BODY_DTO(List<String>, ids), execStartBoard(ids));
+	API_HANDLER_ENDPOINT_AUTH(API_M_PUT, "org/boardmanager/start-board", startBoard, BODY_DTO(List<String>, ids), execStartBoard(ids));
 
 	// 3.1 定义停用公告接口描述
 	API_DEF_ENDPOINT_INFO_AUTH(ZH_WORDS_GETTER("board.end.summary"), endBoard, ListJsonVO<String>::Wrapper, API_TAG);
 	// 3.2 定义停用公告接口处理
-	API_HANDLER_ENDPOINT_AUTH(API_M_PUT, "/board/end-board", endBoard, BODY_DTO(List<String>, ids), execEndBoard(ids));
+	API_HANDLER_ENDPOINT_AUTH(API_M_PUT, "org/boardmanager/end-board", endBoard, BODY_DTO(List<String>, ids), execEndBoard(ids));
 
 private:	//定义接口执行函数
 	StringJsonVO::Wrapper executeAddBoard(const BoardAddDTO::Wrapper& dto,const PayloadDTO& payload);
