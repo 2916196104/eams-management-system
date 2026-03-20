e1ac1896eed36a545831a32915f2cc7bcb768762
YULUN <151115678+feiyangyang11@users.noreply.github.com>
2026-03-20 21:27:19 +0800

    修改文件编码

--- eams-cpp/lib-oatpp/include/Message.h
+++ eams-cpp/lib-oatpp/include/Message.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 /*
  Copyright Zero One Star. All rights reserved.
 
@@ -23,7 +23,7 @@
 #include "ResultStatus.h"
 
 /**
- * ����״̬��ʾ��Ϣ
+ * ????????????
  */
 #ifdef LINUX
 #define RS_UNAUTHORIZED ResultStatus("no login or token expired", 401)
@@ -36,15 +36,15 @@
 #define RS_FAIL ResultStatus("the operation failure", 9999)
 #define RS_SUCCESS ResultStatus("operation is successful")
 #else
-#define RS_UNAUTHORIZED ResultStatus(u8"��δ��¼��TOKEN�Ѿ�����", 401)
-#define RS_FORBIDDEN ResultStatus(u8"û�����Ȩ��", 403)
-#define RS_SERVER_ERROR ResultStatus(u8"����������", 9994)
-#define RS_PARAMS_INVALID ResultStatus(u8"�ϴ������쳣", 9995)
-#define RS_CONTENT_TYPE_ERR ResultStatus(u8"ContentType����", 9996)
-#define RS_API_UN_IMPL ResultStatus(u8"�ӿ���δʵ��", 9997)
-#define RS_SERVER_BUSY ResultStatus(u8"��������æ", 9998)
-#define RS_FAIL ResultStatus(u8"����ʧ��", 9999)
-#define RS_SUCCESS ResultStatus(u8"�����ɹ�")
+#define RS_UNAUTHORIZED ResultStatus(u8"??��?????TOKEN???????", 401)
+#define RS_FORBIDDEN ResultStatus(u8"?????????", 403)
+#define RS_SERVER_ERROR ResultStatus(u8"??????????", 9994)
+#define RS_PARAMS_INVALID ResultStatus(u8"?????????", 9995)
+#define RS_CONTENT_TYPE_ERR ResultStatus(u8"ContentType????", 9996)
+#define RS_API_UN_IMPL ResultStatus(u8"?????��???", 9997)
+#define RS_SERVER_BUSY ResultStatus(u8"?????????", 9998)
+#define RS_FAIL ResultStatus(u8"???????", 9999)
+#define RS_SUCCESS ResultStatus(u8"???????")
 #endif
 
 #endif // !_MESSAGE_
