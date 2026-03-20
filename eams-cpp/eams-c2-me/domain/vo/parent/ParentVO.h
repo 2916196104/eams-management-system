58fbab34a12ff53961a9d4ca7a969296e8378e69
shuishouy <yjtpost@qq.com>
2026-03-20 17:42:03 +0800

    增添query和vo领域模型定义

--- eams-cpp/eams-c2-me/domain/vo/parent/ParentVO.h
+++ eams-cpp/eams-c2-me/domain/vo/parent/ParentVO.h
@@ -1,13 +1,19 @@
 #pragma once
 
+<<<<<<< Updated upstream
 #ifndef _PARENTVO_H_
 #define _PARENTVO_H_
+=======
+#ifndef _PARENT_VO_
+#define _PARENT_VO_
+>>>>>>> Stashed changes
 
 #include "../../GlobalInclude.h"
 #include "../../dto/parent/ParentDTO.h"
 
 #include OATPP_CODEGEN_BEGIN(DTO)
 
+<<<<<<< Updated upstream
 /**
  * 示例显示JsonVO，用于响应给客户端的Json对象
 	家长数据传输对象
@@ -18,3 +24,15 @@ class ParentJsonVO : public JsonVO<ParentDTO::Wrapper> {
 
 #include OATPP_CODEGEN_END(DTO)
 #endif // !_PARENTVO_H_
+=======
+/*
+ * 显示JsonVO，用于响应给客户端的Json对象 
+ */
+class ParentJsonVO : public JsonVO<ParentDTO::Wrapper> {
+  DTO_INIT(ParentJsonVO, JsonVO<ParentDTO::Wrapper>);
+};
+
+#include OATPP_CODEGEN_END(DTO)
+#endif // !_PARENT_VO_
+
+>>>>>>> Stashed changes
