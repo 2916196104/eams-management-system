38cb7a045f2a5355f07c297b2c2eccd40dfb5373
lingluody <1035907499@qq.com>
2026-03-20 18:02:36 +0800

    改了点注释

--- eams-cpp/eams-c6-student/domain/dto/common/CommonDTO.h
+++ eams-cpp/eams-c6-student/domain/dto/common/CommonDTO.h
@@ -55,7 +55,7 @@ class RegistrationPageDTO : public PageDTO<RegistrationDTO::Wrapper> {
 };
 class FileOnlyDTO :public oatpp::DTO {
 	DTO_INIT(FileOnlyDTO, DTO);
-	//API_DTO_FIELD_DEFAULT(oatpp::swagger::Binary, file, ZH_WORDS_GETTER("common.field.file.file"),true);第四个参数多出来了，导致编译错误，暂时注释掉了，还是能跑
+	//API_DTO_FIELD_DEFAULT(oatpp::swagger::Binary, file, ZH_WORDS_GETTER("common.field.file.file"),true);多出第四个参数导致编译错误
 	API_DTO_FIELD_REQUIRE(oatpp::swagger::Binary, file, ZH_WORDS_GETTER("common.field.file.file"), true);
 };
 #include OATPP_CODEGEN_END(DTO)
