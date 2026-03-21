6ef674896d8cfc7e6eaed06a855f66769a134e77
Khalil Leo <3440651823@qq.com>
2026-03-21 11:54:13 +0800

    完成接口编写：组织机构树获取和组织保存功能

--- eams-cpp/eams-c2-org/domain/dto/institution/InstitutionDTO.h
+++ eams-cpp/eams-c2-org/domain/dto/institution/InstitutionDTO.h
@@ -1,18 +1,20 @@
 #pragma once
-
-#ifndef _INSTITUTIONDTO_H_
-#define _INSTITUTIONDTO_H_
+#ifndef _INSTITUTION_DTO_
+#define _INSTITUTION_DTO_
 
 #include "../../GlobalInclude.h"
+#include "../../lib-oatpp/include/ApiHelper.h"
 
 #include OATPP_CODEGEN_BEGIN(DTO)
 
-/*
-	机构数据传输对象
- */
 class InstitutionDTO : public oatpp::DTO {
-  DTO_INIT(InstitutionDTO, DTO);
+    DTO_INIT(InstitutionDTO, DTO);
+
+    API_DTO_FIELD_DEFAULT(String, id, ZH_WORDS_GETTER("institution.field.id"));
+    API_DTO_FIELD_DEFAULT(String, name, ZH_WORDS_GETTER("institution.field.name"));
+    API_DTO_FIELD_DEFAULT(String, parentId, ZH_WORDS_GETTER("institution.field.parentId"));
+    API_DTO_FIELD_DEFAULT(List<Object<InstitutionDTO>>, children, ZH_WORDS_GETTER("institution.field.children"));
 };
 
 #include OATPP_CODEGEN_END(DTO)
-#endif // _INSTITUTIONDTO_H_
+#endif // _INSTITUTION_DTO_
