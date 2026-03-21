91148851a1e81fc436877b8f61ac2f5dea3deca5
xjq <xjq@xjq.cn>
2026-03-21 18:05:53 +0800

    feat: 添加结业学员导入导出接口
    - 定义GraduatedStuApis接口，包含导入、导出、下载模板三个接口
    - 实现GraduatedStuController控制器
    - 定义ImportGraduatedStudentDTO导入参数DTO
    - 清理不需要的结业学员查询相关代码

--- eams-java/eams-j8-stumanager/src/main/java/com/zeroone/star/stumanager/controller/GraduatedStuController.java
+++ eams-java/eams-j8-stumanager/src/main/java/com/zeroone/star/stumanager/controller/GraduatedStuController.java
@@ -1,14 +1,53 @@
 package com.zeroone.star.stumanager.controller;
 
+import com.zeroone.star.project.j8.stumanager.GraduatedStuApis;
+import com.zeroone.star.project.vo.JsonVO;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
- * 结业学员
+ * 结业学员控制器
  */
 @Api(tags = "结业学员")
 @RequestMapping("/stu/graduated-stu")
 @RestController
-public class GraduatedStuController {
+public class GraduatedStuController implements GraduatedStuApis {
+
+    /**
+     * 导出结业学员列表
+     */
+    @GetMapping("/export")
+    @ApiOperation(value = "导出结业学员列表", notes = "导出Excel格式的结业学员列表")
+    @Override
+    public void exportGraduatedStudents(HttpServletResponse response) {
+        // 导出功能实现
+    }
+
+    /**
+     * 下载导入模板
+     */
+    @GetMapping("/download-template")
+    @ApiOperation(value = "下载导入模板", notes = "下载结业学员导入模板Excel文件")
+    @Override
+    public void downloadImportTemplate(HttpServletResponse response) {
+        // 下载模板实现
+    }
+
+    /**
+     * 导入结业学员
+     */
+    @PostMapping("/import")
+    @ApiOperation(value = "导入结业学员", notes = "通过Excel文件批量导入结业学员")
+    @Override
+    public JsonVO<String> importGraduatedStudents(@RequestParam("file") MultipartFile file) {
+        JsonVO<String> jsonVO = new JsonVO<>();
+        jsonVO.setData("导入成功");
+        jsonVO.setCode(10000);
+        jsonVO.setMessage("success");
+        return jsonVO;
+    }
 }
