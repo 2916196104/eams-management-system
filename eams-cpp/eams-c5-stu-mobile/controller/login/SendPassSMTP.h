dfec9889c5bd011122c53f02a2ecaf5063a895a6
pikachu <2485175197@qq.com>
2026-03-21 17:47:23 +0800

    完成发送密码验证与兑换物品详情接口的二次修改

--- eams-cpp/eams-c5-stu-mobile/controller/login/SendPassSMTP.h
+++ eams-cpp/eams-c5-stu-mobile/controller/login/SendPassSMTP.h
@@ -25,19 +25,20 @@ public:		//定义接口
 		//定义响应参数格式
 		API_DEF_ADD_RSP_JSON(StringJsonVO::Wrapper);
 		//或使用API_DEF_ADD_RSP_JSON_WRAPPER(StringJsonVO);来代替
-
+		API_DEF_ADD_TAG(ZH_WORDS_GETTER("credit_mall.tags.t4"));
 		//定义请求参数格式使用宏定义API_DEF_ADD_PAGE_PARAMS();来代替
 		API_DEF_ADD_QUERY_PARAMS(String, "phone", ZH_WORDS_GETTER("spsmtp.phone"), "13999999999", false);
 
 	}
 
-	ENDPOINT(API_M_GET, "/spsmtp", SendSMTP, QUERIES(QueryParams, params), API_HANDLER_AUTH_PARAME) {
+	ENDPOINT(API_M_POST, "/c5/login/sendpass", SendSMTP, QUERIES(QueryParams, params), API_HANDLER_AUTH_PARAME) {
 		//调用框架宏定义来快速解析参数类型，宏定义使用方法：	API_HANDLER_QUERY_PARAM（存储变量，参数类型，变量)
 		API_HANDLER_QUERY_PARAM(query, PageQuery, params);
 
 		//API_HANDLER_RESP_VO(vo指针)使用宏定义来代替return createResponse(Status::CODE_200, "Hello World!");		响应结果
 		API_HANDLER_RESP_VO(executeSendSMTP(query));
 	}
+
 
 private:	 //定义接口执行函数
 	StringJsonVO::Wrapper executeSendSMTP(const PageQuery::Wrapper& query);
