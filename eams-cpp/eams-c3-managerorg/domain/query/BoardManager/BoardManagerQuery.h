16d0791b809d60d4a8ce81b6e905c9e0b0b59616
xy <a2758014004@qq.com>
2026-03-21 23:55:00 +0800

    完成公告列表查询

--- eams-cpp/eams-c3-managerorg/domain/query/BoardManager/BoardManagerQuery.h
+++ eams-cpp/eams-c3-managerorg/domain/query/BoardManager/BoardManagerQuery.h
@@ -17,13 +17,13 @@ class BoardQuery : public PageQuery
 	// 定义初始化
 	DTO_INIT(BoardQuery, PageQuery);
 	// 公告标题
-	API_DTO_FIELD_DEFAULT(String, boardTitle, ZH_WORDS_GETTER("user.field.boardTitle"));
+	API_DTO_FIELD_DEFAULT(String, boardTitle, ZH_WORDS_GETTER("board.field.boardTitle"));
 	// 公告发布者名称
-	API_DTO_FIELD_DEFAULT(String, boardPublisherName, ZH_WORDS_GETTER("user.field.boardPublisherName"));
+	API_DTO_FIELD_DEFAULT(String, boardPublisherName, ZH_WORDS_GETTER("board.field.boardPublisherName"));
 	// 公告发布者id
-	API_DTO_FIELD_DEFAULT(String, boardPublisherId, ZH_WORDS_GETTER("user.field.boardPublisherId"));
+	API_DTO_FIELD_DEFAULT(String, boardPublisherId, ZH_WORDS_GETTER("board.field.boardPublisherId"));
 	// 公告类型
-	API_DTO_FIELD_DEFAULT(Int8, boardType, ZH_WORDS_GETTER("user.field.boardType"));
+	API_DTO_FIELD_DEFAULT(Int8, boardType, ZH_WORDS_GETTER("board.field.boardType"));
 };
 
 #include OATPP_CODEGEN_END(DTO)
