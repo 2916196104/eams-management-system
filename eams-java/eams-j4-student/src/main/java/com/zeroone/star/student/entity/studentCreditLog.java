250814a35b2d0c9a87c6b655f9ff8f75d3672ef4
MRME39 <mrme393939@gmail.com>
2026-03-21 12:44:19 +0800

    MRME39-3/17-完善了数据类型

---
+++ eams-java/eams-j4-student/src/main/java/com/zeroone/star/student/entity/studentCreditLog.java
@@ -0,0 +1,30 @@
+package com.zeroone.star.student.entity;
+
+import java.time.LocalDateTime;
+
+/**
+ * student_Credit_Log
+ */
+public class studentCreditLog {
+    private int id;
+
+    private int studentId;
+
+    private int userId;
+
+    private int credit;
+
+    private int current_credit;
+
+    private int changeType;
+
+    private LocalDateTime addTime;
+
+    private String remark;
+
+    private int sourceId;
+
+    private int staffId;
+
+    private int schoolId;
+}
---
+++ eams-java/eams-j4-student/src/main/java/com/zeroone/star/student/entity/studentLessonCountLog.java
@@ -0,0 +1,25 @@
+package com.zeroone.star.student.entity;
+
+import java.time.LocalDateTime;
+
+public class studentLessonCountLog {
+    private int id;
+
+    private int studentId;
+
+    private int courseId;
+
+    private int lessonId;
+
+    private int changeCount;
+
+    private int remainingCount;
+
+    private LocalDateTime addTime;
+
+    private String remark;
+
+    private int staff_id;
+
+    private int stage;
+}
