dd962e386ad8196a75a53c74d4da10bb2bcabcd2
yeyue <1923780217@qq.com>
2026-03-23 20:35:59 +0800

    新增实体“ContactRecord”  “Student”

---
+++ eams-java/eams-j3-data/src/main/java/com/zeroone/star/eamsj3data/entity/Student.java
@@ -0,0 +1,46 @@
+package com.zeroone.star.eamsj3data.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * <p>
+ * 学员实体
+ * </p>
+ * @author yeyue
+ * @version 1.0.0
+ */
+@Getter
+@Setter
+@TableName("student")
+public class Student implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    @TableId(value = "id", type = IdType.ASSIGN_ID)
+    private Long id;
+
+    /**
+     * 阶段状态
+     */
+    private Integer stage;
+
+    /**
+     * 生日
+     */
+    private LocalDate birthday;
+
+    /**
+     * 删除标记
+     */
+    private Integer deleted;
+}
