ffe379072a99847b945911ae9085f58c584456de
ll <2856880955@qq.com>
2026-03-23 20:31:30 +0800

    对修改学员头像和查询学员报名记录做改动，具体为将修改学员头像接口改为对图像路径的修改；为查询学员报名记录接口添加分类参数

--- eams-cpp/eams-c6-student/controller/common/CommonController.h
+++ eams-cpp/eams-c6-student/controller/common/CommonController.h
@@ -12,7 +12,7 @@
 class CommonController : public oatpp::web::server::api::ApiController {
 	API_ACCESS_DECLARE(CommonController);
 public:
-//获取学员详情
+	//获取学员详情
 	API_DEF_ENDPOINT_INFO_AUTH(
 		ZH_WORDS_GETTER("common.interface.get-student-by-id"), 
 		getStudentById, StudentJsonVO::Wrapper,
@@ -24,36 +24,21 @@ public:
 	)
 	API_HANDLER_ENDPOINT_AUTH(
 		API_M_GET,
-		"/c3/common/student/get-by-id", 
+		"/c6/common/student/get-by-id", 
 		getStudentById,
 		QUERY(String, id), 
 		executeGetStudentById(id)
 	);
-
-	//下载学员头像
-	API_DEF_ENDPOINT_INFO(
-		ZH_WORDS_GETTER("common.interface.download-student-picture"), loadStudentPicturebyPicturePath, Void, API_TAG,
-		API_DEF_ADD_QUERY_PARAMS(String, "picturePath", ZH_WORDS_GETTER("common.field.student.picture-path"), "file/test.jpg", true);
-	);
-	API_HANDLER_ENDPOINT_OPTION(
-		API_M_GET, 
-		"/c3/common/student/download-student-picture", 
-		loadStudentPicturebyPicturePath,
-		QUERY(String, picturePath), 
-		return executeLoadStudentPicturebyPicturePath(picturePath);
-	);
-
 	//修改学员头像
 	ENDPOINT_INFO(modifyStudentPicture) {
 		API_DEF_ADD_AUTH();//添加权限验证
 		API_DEF_ADD_TAG(API_TAG);//将接口添加到分组
 		API_DEF_ADD_TITLE(ZH_WORDS_GETTER("common.interface.modify-student-picture"));//添加接口描述
-		API_DEF_ADD_PATH_PARAMS(String, "id", ZH_WORDS_GETTER("common.field.student.id"), "123", true);//添加参数
-		API_DEF_ADD_FILE_FORM(FileOnlyDTO::Wrapper);
+		API_DEF_ADD_PATH_PARAMS(String, "picturePath", ZH_WORDS_GETTER("common.field.student.picture-path"), "123", true);//添加参数
 		API_DEF_ADD_RSP_JSON_WRAPPER(StringJsonVO);//添加返回数据包装
 	}
-    ENDPOINT(API_M_POST, "/c3/common/student/modify-picture/{id}",modifyStudentPicture,PATH(String,id), REQUEST(std::shared_ptr<IncomingRequest>, request), API_HANDLER_AUTH_PARAME) {
-		API_HANDLER_RESP_VO(executeModifyStudentPicture(id,request));
+    ENDPOINT(API_M_POST, "/c6/common/student/modify-picture/{picturePath}",modifyStudentPicture,PATH(String,picturePath),API_HANDLER_AUTH_PARAME) {
+		API_HANDLER_RESP_VO(executeModifyStudentPicture(picturePath));
 	}
 
 	//查询学员报名记录
@@ -61,10 +46,11 @@ public:
 		API_DEF_ADD_AUTH();//添加权限验证
 		API_DEF_ADD_TAG(API_TAG);//将接口添加到分组
         API_DEF_ADD_TITLE(ZH_WORDS_GETTER("common.interface.query-student-registration-record"));//添加接口描述
+		API_DEF_ADD_QUERY_PARAMS(Int64, "id", ZH_WORDS_GETTER("common.field.student.id"), 213423231, true);
 		API_DEF_ADD_PAGE_PARAMS();//添加分页参数
 		API_DEF_ADD_RSP_JSON_WRAPPER(RegistrationPageJsonVO);//添加返回数据包装
 	}
-	ENDPOINT(API_M_GET, "/c3/common/registration-recordf/query-by-page", queryRegistrationRecordByPage, QUERIES(QueryParams, queryParams),API_HANDLER_AUTH_PARAME) {
+	ENDPOINT(API_M_GET, "/c6/common/registration-recordf/query-by-page", queryRegistrationRecordByPage, QUERIES(QueryParams, queryParams),API_HANDLER_AUTH_PARAME) {
 		API_HANDLER_QUERY_PARAM(query, RegistrationPageQuery, queryParams);//获取参数
 		API_HANDLER_RESP_VO(executeQueryRegistrationRecordByPage(query));
 	}	
@@ -76,7 +62,7 @@ public:
 		API_DEF_ADD_PATH_PARAMS(String, "id", ZH_WORDS_GETTER("common.field.student.id"), "123", true);//添加参数
 		API_DEF_ADD_RSP_JSON_WRAPPER(CourseCountJsonVO);//添加返回数据包装
 	}
-	ENDPOINT(API_M_GET, "/c3/common/interface/get-student-course-count", getCourseCountData, PATH(String,id), API_HANDLER_AUTH_PARAME) {
+	ENDPOINT(API_M_GET, "/c6/common/interface/get-student-course-count", getCourseCountData, PATH(String,id), API_HANDLER_AUTH_PARAME) {
 		API_HANDLER_RESP_VO(executeGetCourseCountData(id));
 	}
 	//保存学员
@@ -87,17 +73,15 @@ public:
 		API_DEF_ADD_PATH_PARAMS(String, "id", ZH_WORDS_GETTER("common.field.student.id"), "123", true);//添加参数
 		API_DEF_ADD_RSP_JSON_WRAPPER(StringJsonVO);//添加返回数据包装
 	}
-	ENDPOINT(API_M_GET, "/c3/common/interface/save-student", SaveStudent, PATH(String, id), BODY_DTO(SaveStudentDTO::Wrapper, dto), API_HANDLER_AUTH_PARAME) {
+	ENDPOINT(API_M_GET, "/c6/common/interface/save-student", SaveStudent, PATH(String, id), BODY_DTO(SaveStudentDTO::Wrapper, dto), API_HANDLER_AUTH_PARAME) {
 		API_HANDLER_RESP_VO(executeSaveStudent(id));
 	}
-
-
 	// 3.1 学员报名课程申请描述 
 	API_DEF_ENDPOINT_INFO_AUTH(ZH_WORDS_GETTER("common.interface.enrollstudentCourse"), EnrollStudentCourselist, StringJsonVO::Wrapper, API_TAG);
 	// 3.2 学员报名课程申请处理
 	API_HANDLER_ENDPOINT_AUTH(
 		API_M_POST,
-		"/c3/common/enrollment/student/apply",
+		"/c6/common/enrollment/student/apply",
 		EnrollStudentCourselist,
 		BODY_DTO(RegistrationApplyDTO::Wrapper, dto),
 		executeEnrollStudentCourse(dto, authObject->getPayload())
@@ -108,7 +92,7 @@ public:
 	// 3.2 学员阶段设置处理
 	API_HANDLER_ENDPOINT_AUTH(
 		API_M_PUT,
-		"/c3/common/enrollment/student/stage",
+		"/c6/common/enrollment/student/stage",
 		StudentStage,
 		BODY_DTO(StudentStageUpdateDTO::Wrapper, dto),
 		executeModifyStudentStage(dto, authObject->getPayload())
@@ -116,21 +100,14 @@ public:
 
 
 private:
-	
-
-
-
 	StudentJsonVO::Wrapper executeGetStudentById(const String& id){
 		return StudentJsonVO::createShared();
 	}
-    StringJsonVO::Wrapper executeModifyStudentPicture(const String& id,std::shared_ptr<IncomingRequest>request){
+    StringJsonVO::Wrapper executeModifyStudentPicture(const String& picturePath){
 		return StringJsonVO::createShared();
 	}
 	RegistrationPageJsonVO::Wrapper executeQueryRegistrationRecordByPage(const RegistrationPageQuery::Wrapper& query){
 		return RegistrationPageJsonVO::createShared();
-	}
-	std::shared_ptr<OutgoingResponse> executeLoadStudentPicturebyPicturePath(const String& picturePath){
-		return nullptr;
 	}
 	CourseCountJsonVO::Wrapper executeGetCourseCountData(const String& id) {
 		return CourseCountJsonVO::createShared();
@@ -149,10 +126,6 @@ private:
 
 		return StringJsonVO::createShared();
 	}
-
-
-
-
 };
 
 #undef API_TAG
--- eams-cpp/eams-c6-student/domain/do/student/StudentDO.h
+++ eams-cpp/eams-c6-student/domain/do/student/StudentDO.h
@@ -1,1 +1,45 @@
 #pragma once
+#ifnef STUDENTDO_H
+#define STUDENTDO_H 
+#include "../DoInclude.h"
+class StaffDO : public BaseDO {
+public:
+	// 职工id
+	MYSQL_SYNTHESIZE(int, id, Id);
+	// 职工姓名
+	MYSQL_SYNTHESIZE(string, name, Name);
+	// 手机号
+	MYSQL_SYNTHESIZE(string, mobile, Mobile);
+	// 微信登录id
+	MYSQL_SYNTHESIZE(int, wxAccessId, WxAccessId);
+	// email
+	MYSQL_SYNTHESIZE(string, email, Email);
+	//生日
+    MYSQL_SYNTHESIZE(string, birthday, BirthDay);
+	//性别
+    MYSQL_SYNTHESIZE(int, sex, Sex);
+	//身份证
+    MYSQL_SYNTHESIZE(string, idCard, IdCard);
+	//毕业学校
+    MYSQL_SYNTHESIZE(string, school, School);
+	//学历
+    MYSQL_SYNTHESIZE(int, degree, Degree);
+	//备注
+    MYSQL_SYNTHESIZE(string, remark, Remark);
+	//在职状态
+    MYSQL_SYNTHESIZE(int, state, State);
+	//是否是管理者
+    MYSQL_SYNTHESIZE(int, isManager, IsManager);
+	//头像
+    MYSQL_SYNTHESIZE(string, headImg, HeadImg);
+	//入职日期
+    MYSQL_SYNTHESIZE(string, hireDate, HireDate);
+
+};
+
+
+
+
+
+
+#endif
