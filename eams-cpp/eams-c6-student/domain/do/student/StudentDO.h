919bfd13d92851fd7fde693a4639435c864df0f5
ll <2856880955@qq.com>
2026-03-24 16:45:04 +0800

    修改StaffDO类的字段错误bug

--- eams-cpp/eams-c6-student/domain/do/student/StudentDO.h
+++ eams-cpp/eams-c6-student/domain/do/student/StudentDO.h
@@ -15,49 +15,49 @@ public:
 	// email
 	MYSQL_SYNTHESIZE(string, email, Email);
 	//生日
-    MYSQL_SYNTHESIZE(string, birthday, BirthDay);
+	MYSQL_SYNTHESIZE(string, birthday, BirthDay);
 	//性别
-    MYSQL_SYNTHESIZE(int, sex, Sex);
+	MYSQL_SYNTHESIZE(int, sex, Sex);
 	//身份证
-    MYSQL_SYNTHESIZE(string, idCard, IdCard);
+	MYSQL_SYNTHESIZE(string, idCard, IdCard);
 	//毕业学校
-    MYSQL_SYNTHESIZE(string, school, School);
+	MYSQL_SYNTHESIZE(string, school, School);
 	//学历
-    MYSQL_SYNTHESIZE(int, degree, Degree);
+	MYSQL_SYNTHESIZE(int, degree, Degree);
 	//备注
-    MYSQL_SYNTHESIZE(string, remark, Remark);
+	MYSQL_SYNTHESIZE(string, remark, Remark);
 	//在职状态
-    MYSQL_SYNTHESIZE(int, state, State);
+	MYSQL_SYNTHESIZE(int, state, State);
 	//是否是管理者
-    MYSQL_SYNTHESIZE(int, isManager, IsManager);
+	MYSQL_SYNTHESIZE(int, isManager, IsManager);
 	//头像
-    MYSQL_SYNTHESIZE(string, headImg, HeadImg);
+	MYSQL_SYNTHESIZE(string, headImg, HeadImg);
 	//入职日期
-    MYSQL_SYNTHESIZE(string, hireDate, HireDate);
+	MYSQL_SYNTHESIZE(string, hireDate, HireDate);
 	//离职日期
-	MYSQL_SYNTHESIZE(string, fireData, FireDate);
+	MYSQL_SYNTHESIZE(string, fireDate, FireDate);
 	//介绍
-    MYSQL_SYNTHESIZE(string, introduction, Introduction);
+	MYSQL_SYNTHESIZE(string, introduction, Introduction);
 	//创建时间
 	MYSQL_SYNTHESIZE(string, addTime, AddTime);
 	//编辑时间
-    MYSQL_SYNTHESIZE(string, editTime, EditTime);
+	MYSQL_SYNTHESIZE(string, editTime, EditTime);
 	//创建人
-    MYSQL_SYNTHESIZE(string, creator, Creator);
+	MYSQL_SYNTHESIZE(string, creator, Creator);
 	//编辑人
-    MYSQL_SYNTHESIZE(string, editor, Editor);
+	MYSQL_SYNTHESIZE(string, editor, Editor);
 	//是否删除
-    MYSQL_SYNTHESIZE(int, deleted, Deleted);
+	MYSQL_SYNTHESIZE(int, deleted, Deleted);
 	//密码
 	MYSQL_SYNTHESIZE(string, password, Password);
 	//课时费
-    MYSQL_SYNTHESIZE(int, classFee, ClassFee);
+	MYSQL_SYNTHESIZE(int, classFee, ClassFee);
 	//助教费
-    MYSQL_SYNTHESIZE(int, assistantFee, AssistantFee);
+	MYSQL_SYNTHESIZE(int, assistantFee, AssistantFee);
 	//staff所属组织id
-    MYSQL_SYNTHESIZE(long long, orgId, OrgId);
+	MYSQL_SYNTHESIZE(long long, orgId, OrgId);
 	//是否内部人员
-    MYSQL_SYNTHESIZE(int, isInner, IsInner);
+	MYSQL_SYNTHESIZE(int, isInner, IsInner);
 public:
 	StaffDO() : BaseDO("staff")
 	{
@@ -75,19 +75,19 @@ public:
 		MYSQL_ADD_FIELD("state", "i", state);
 		MYSQL_ADD_FIELD("isManager", "i", isManager);
 		MYSQL_ADD_FIELD("headImg", "s", headImg);
-		MYSQL_ADD_FIELD("hireData", "s", hireData);
-        MYSQL_ADD_FIELD("fireData", "s", fireData);
-        MYSQL_ADD_FIELD("introduction", "s", introduction);
-        MYSQL_ADD_FIELD("addTime", "s", addTime);
-        MYSQL_ADD_FIELD("editTime", "s", editTime);
-        MYSQL_ADD_FIELD("creator", "s", creator);
-        MYSQL_ADD_FIELD("editor", "s", editor);
-        MYSQL_ADD_FIELD("deleted", "i", deleted);
-        MYSQL_ADD_FIELD("password", "s", password);
-        MYSQL_ADD_FIELD("classFee", "i", classFee);
-        MYSQL_ADD_FIELD("assistantFee", "i", assistantFee);
-        MYSQL_ADD_FIELD("orgId", "i", orgId);
-        MYSQL_ADD_FIELD("isInner", "i", isInner);
+		MYSQL_ADD_FIELD("hireDate", "s", hireDate);
+		MYSQL_ADD_FIELD("fireDate", "s", fireDate);
+		MYSQL_ADD_FIELD("introduction", "s", introduction);
+		MYSQL_ADD_FIELD("addTime", "s", addTime);
+		MYSQL_ADD_FIELD("editTime", "s", editTime);
+		MYSQL_ADD_FIELD("creator", "s", creator);
+		MYSQL_ADD_FIELD("editor", "s", editor);
+		MYSQL_ADD_FIELD("deleted", "i", deleted);
+		MYSQL_ADD_FIELD("password", "s", password);
+		MYSQL_ADD_FIELD("classFee", "i", classFee);
+		MYSQL_ADD_FIELD("assistantFee", "i", assistantFee);
+		MYSQL_ADD_FIELD("orgId", "i", orgId);
+		MYSQL_ADD_FIELD("isInner", "i", isInner);
 	}
 };
 
