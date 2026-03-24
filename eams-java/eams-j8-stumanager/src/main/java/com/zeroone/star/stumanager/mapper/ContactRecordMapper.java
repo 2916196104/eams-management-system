07b460304fcb9ab1297ea3d3791520ca5f2cc88d
heziyan <heziyan@heziyan.cn>
2026-03-24 18:25:27 +0800

    完成了业务逻辑代码，调整了controller接口

--- eams-java/eams-j8-stumanager/src/main/java/com/zeroone/star/stumanager/mapper/ContactRecordMapper.java
+++ eams-java/eams-j8-stumanager/src/main/java/com/zeroone/star/stumanager/mapper/ContactRecordMapper.java
@@ -3,6 +3,9 @@ package com.zeroone.star.stumanager.mapper;
 import com.zeroone.star.stumanager.entity.ContactRecord;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,5 +17,6 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface ContactRecordMapper extends BaseMapper<ContactRecord> {
-
+    List<ContactRecord> selectByStudentId(@Param("studentId") Long studentId);
 }
+
