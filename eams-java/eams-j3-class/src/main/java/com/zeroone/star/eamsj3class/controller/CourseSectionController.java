4fc1073fb93ba9a474def67396adb85ae574c432
shangchang <2291265410@qq.com>
2026-03-24 13:30:30 +0800

    实现课程章节的功能

--- eams-java/eams-j3-class/src/main/java/com/zeroone/star/eamsj3class/controller/CourseSectionController.java
+++ eams-java/eams-j3-class/src/main/java/com/zeroone/star/eamsj3class/controller/CourseSectionController.java
@@ -1,5 +1,6 @@
 package com.zeroone.star.eamsj3class.controller;
 
+import com.zeroone.star.eamsj3class.service.CourseSectionService;
 import com.zeroone.star.project.dto.PageDTO;
 import com.zeroone.star.project.dto.j3.course.CourseSectionDTO;
 import com.zeroone.star.project.j3.course.CourseSectionApis;
@@ -10,6 +11,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.validation.Valid;
 
 @RestController
@@ -17,25 +19,30 @@ import javax.validation.Valid;
 @Api(tags = "课程章节设置")
 public class CourseSectionController implements CourseSectionApis {
 
+    @Resource
+    private CourseSectionService sectionService;
     @Override
-    @GetMapping
+    @GetMapping("/list")
     @ApiOperation("获取关联章节列表（条件+分页）")
     public JsonVO<PageDTO<CourseSectionDTO>> queryPage(@Valid CourseSectionPageQuery query) {
-        return null;
+        PageDTO<CourseSectionDTO> pageResult = sectionService.queryPage(query);
+        return JsonVO.success(pageResult);
     }
 
     @Override
-    @PostMapping
+    @PostMapping("/add")
     @ApiOperation("添加章节")
     public JsonVO<Long> addSection(@Valid @RequestBody CourseSectionDTO dto) {
-        return null;
+        Long sectionId = sectionService.addSection(dto);
+        return JsonVO.success(sectionId);
     }
 
     @Override
-    @DeleteMapping
+    @DeleteMapping("/delete/{id}")
     @ApiOperation("移除章节")
     @ApiImplicitParam(name = "id", value = "章节ID", required = true)
     public JsonVO<Void> deleteSection(@PathVariable Long id) {
-        return null;
+        sectionService.deleteSection(id);
+        return JsonVO.success(null);
     }
 }
