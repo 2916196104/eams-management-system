14a4f8dc50c392f561a69b095cff32f5a19b7a7c
zjw <3166515516@qq.com>
2026-03-24 19:50:45 +0800

    接口定义

--- eams-cpp/eams-c2-org/controller/institution/InstitutionController.cpp
+++ eams-cpp/eams-c2-org/controller/institution/InstitutionController.cpp
@@ -1,2 +1,24 @@
-#include "stdafx.h"
 #include "InstitutionController.h"
+
+StringJsonVO::Wrapper InstitutionController::execQueryInstitutionList()
+{
+    auto jvo = StringJsonVO::createShared();
+    jvo->success(ZH_WORDS_GETTER("institution.list.tips"));
+    return jvo;
+}
+
+StringJsonVO::Wrapper InstitutionController::execQueryInstitutionDetail(const Int64& id)
+{
+    auto jvo = StringJsonVO::createShared();
+    std::string msg = std::string(ZH_WORDS_GETTER("institution.detail.tips")) + ": " + std::to_string(id);
+    jvo->success(msg);
+    return jvo;
+}
+
+BooleanJsonVO::Wrapper InstitutionController::execDeleteInstitution(const String& id)
+{
+    auto jvo = BooleanJsonVO::createShared();
+    jvo->success(true);
+    jvo->message = ZH_WORDS_GETTER("institution.delete.tips");
+    return jvo;
+}
--- eams-cpp/eams-c2-org/controller/institution/InstitutionController.h
+++ eams-cpp/eams-c2-org/controller/institution/InstitutionController.h
@@ -1,29 +1,73 @@
 #pragma once
 
-#ifndef _INSTITUTION_CONTROLLER_ 
-#define _INSTITUTION_CONTROLLER_ 
 
-#include "domain/vo/BaseJsonVO.h"
-#include "ApiHelper.h"
+#include "../lib-oatpp/include/ApiHelper.h"
 #include "ServerInfo.h"
+#include "Macros.h"
+#include "domain/vo/BaseJsonVO.h"
+//#include "domain/query/PageQuery.h"
 
-// 1 定义API控制器使用宏
+#define API_TAG "机构管理"
+
 #include OATPP_CODEGEN_BEGIN(ApiController)
-#define API_TAG ZH_WORDS_GETTER("sample.tags")
 
-/*
+//#define API_TAG ZH_WORDS_GETTER("institution.tags.management")
+
+/**
  * 机构控制器
  */
-class InstitutionController : public oatpp::web::server::api::ApiController // 1 继承控制器
+class InstitutionController : public oatpp::web::server::api::ApiController
 {
-  // 2 定义控制器访问入口
-  API_ACCESS_DECLARE(InstitutionController);
-  // 3 定义接口
-public:
+    API_ACCESS_DECLARE(InstitutionController);
 
-private: // 定义接口执行函数
+public:
+    // ==================== 1. 获取机构列表 ====================
+    ENDPOINT_INFO(queryInstitutionList) {
+        API_DEF_ADD_TITLE(ZH_WORDS_GETTER("institution.list.summary"));
+        API_DEF_ADD_AUTH();
+        API_DEF_ADD_RSP_JSON_WRAPPER(StringJsonVO);
+        API_DEF_ADD_TAG(API_TAG);
+        API_DEF_ADD_PAGE_PARAMS();
+        API_DEF_ADD_QUERY_PARAMS(Int64, "pid", ZH_WORDS_GETTER("institution.field.pid"), 0LL, false);
+        API_DEF_ADD_QUERY_PARAMS(Int32, "type", ZH_WORDS_GETTER("institution.field.type"), 0, false);
+        API_DEF_ADD_QUERY_PARAMS(String, "name", ZH_WORDS_GETTER("institution.field.name"), "", false);
+    }
+    ENDPOINT(API_M_GET, "/org/institution/list", queryInstitutionList,
+        QUERIES(QueryParams, queryParams), API_HANDLER_AUTH_PARAME) {
+        API_HANDLER_RESP_VO(execQueryInstitutionList());
+    }
+
+    // ==================== 2. 获取机构详情 ====================
+    ENDPOINT_INFO(queryInstitutionDetail) {
+        API_DEF_ADD_TITLE(ZH_WORDS_GETTER("institution.detail.summary"));
+        API_DEF_ADD_AUTH();
+        API_DEF_ADD_RSP_JSON_WRAPPER(StringJsonVO);
+        API_DEF_ADD_TAG(API_TAG);
+        API_DEF_ADD_QUERY_PARAMS(Int64, "id", ZH_WORDS_GETTER("institution.field.id"), 1LL, true);
+    }
+    ENDPOINT(API_M_GET, "/org/institution/detail", queryInstitutionDetail,
+        QUERY(Int64, id), API_HANDLER_AUTH_PARAME) {
+        API_HANDLER_RESP_VO(execQueryInstitutionDetail(id));
+    }
+
+    // ==================== 3. 删除机构 ====================
+    ENDPOINT_INFO(deleteInstitution) {
+        API_DEF_ADD_TITLE(ZH_WORDS_GETTER("institution.delete.summary"));
+        API_DEF_ADD_AUTH();
+        API_DEF_ADD_RSP_JSON_WRAPPER(BooleanJsonVO);
+        API_DEF_ADD_TAG(API_TAG);
+    }
+    ENDPOINT(API_M_POST, "/org/institution/delete", deleteInstitution,
+        BODY_STRING(String, id), API_HANDLER_AUTH_PARAME) {
+        API_HANDLER_RESP_VO(execDeleteInstitution(id));
+    }
+
+private:
+    StringJsonVO::Wrapper execQueryInstitutionList();
+    StringJsonVO::Wrapper execQueryInstitutionDetail(const Int64& id);
+    BooleanJsonVO::Wrapper execDeleteInstitution(const String& id);
 };
 
 #undef API_TAG
-#include OATPP_CODEGEN_END(ApiController) //<- End Codegen
-#endif // !_POSITION_CONTROLLER_
+
+#include OATPP_CODEGEN_END(ApiController)
