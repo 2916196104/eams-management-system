b939e3e7c454228613a558cb753711cea6da3da0
Evean <3507266933@qq.com>
2026-03-25 13:56:13 +0800

    完成notice表的dao文件实现

---
+++ eams-cpp/eams-c3-managerorg/dao/Board/NoticeDao.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#ifndef _NOTICE_DAO_
+#define _NOTICE_DAO_
+
+#include "BaseDAO.h"
+#include "../../domain/do/Board/NoticeDO.h"
+#include "../../domain/query/BoardManager/BoardManagerQuery.h"
+
+/**
+ * 通知设置数据访问对象
+ */
+class NoticeDAO : public BaseDAO
+{
+public:
+    // 查询所有通知配置
+    std::list<PtrNoticeSettingDO> selectAll();
+
+    // 根据ID查询通知配置
+    PtrNoticeSettingDO selectById(int id);
+
+    // 根据通知编码（code）查询通知配置
+    PtrNoticeSettingDO selectByCode(const std::string& code);
+};
+
+#endif // !_NOTICE_DAO_
