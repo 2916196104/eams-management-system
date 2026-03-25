1d383f3c648f747cf56a92f8bad76f5d79a20ff6
lilixiangfa <2668990525@qq.com>
2026-03-25 15:03:32 +0800

    再次更改manual

--- eams-cpp/eams-c2-me/controller/parent/ParentController.h
+++ eams-cpp/eams-c2-me/controller/parent/ParentController.h
@@ -4,12 +4,8 @@
 #define _PARENT_CONTROLLER_ 
 
 #include "domain/vo/BaseJsonVO.h"
-#include "domain/dto/parent/ParentDTO.h"
-#include "domain/vo/ParentVO.h"
-#include "domain/query/PageQuery.h"
 #include "ApiHelper.h"
 #include "ServerInfo.h"
-#include "dao/parentDao.h"
 
 // 1 定义API控制器使用宏
 #include OATPP_CODEGEN_BEGIN(ApiController)
@@ -20,53 +16,28 @@
  */
 class ParentController : public oatpp::web::server::api::ApiController // 1 继承控制器
 {
-  // 2 定义控制器访问入口
-  API_ACCESS_DECLARE(ParentController);
-
-  // 3 定义接口
+	// 2 定义控制器访问入口
+	API_ACCESS_DECLARE(ParentController);
+	// 3 定义接口
 public:
-    //获取说明列表接口的说明
-    API_DEF_ENDPOINT_INFO_QUERY_AUTH(
-        ZH_WORDS_GETTER("help.querylist"), // 接口标题
-        getList, // 端点函数名
-        ListQuery, // Query类型（自动生成参数文档）
-        UsageGuideJsonVO::Wrapper, // 响应类型
-        ZH_WORDS_GETTER("help.querylist") // 标签
-    );
 
-    //获取说明列表中某一详细信息接口的说明
-    API_DEF_ENDPOINT_INFO_QUERY_AUTH(
-        ZH_WORDS_GETTER("help.querydetail"), // 接口标题
-        getDetail, // 端点函数名
-        DetailQuery, // Query类型（自动生成参数文档）
-        UsageDetailJsonVO::Wrapper, // 响应类型
-        ZH_WORDS_GETTER("help.querydetail") // 标签
-    );
-      
-    //获取说明列表接口
-    API_HANDLER_ENDPOINT_QUERY_AUTH(
-        API_M_GET, // HTTP方法：GET
-        "/me/parent/getList", // 路径
-        getList, // 函数名
-        ListQuery, // Query类型（自动解析参数）
-        executeQueryAll(query) // 调用执行方法
-    );
-
-    //获取说明列表中某一详细信息接口
-    API_HANDLER_ENDPOINT_AUTH(
-        API_M_GET,// HTTP方法：GET
-        "/me/parent/getDetail",// 路径
-        getDetail,// Query类型（自动解析参数）
-        QUERY(String, id), // 接收单个查询参数
-        executeQueryOne(id) // 调用执行方法
-    );
-
-private: 
-    // 定义接口执行函数
-    UsageGuideJsonVO::Wrapper executeQueryAll(const ListQuery::Wrapper& query);
-    UsageDetailJsonVO::Wrapper executeQueryOne(const String& id);
+private: // 定义接口执行函数
 };
 
 #undef API_TAG
 #include OATPP_CODEGEN_END(ApiController) //<- End Codegen
 #endif // !_POSITION_CONTROLLER_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- eams-cpp/eams-c2-me/domain/dto/parent/ParentDTO.h
+++ eams-cpp/eams-c2-me/domain/dto/parent/ParentDTO.h
@@ -4,91 +4,15 @@
 #define _PARENTDTO_H_
 
 #include "../../GlobalInclude.h"
-#include "ApiHelper.h"
-#include "ServerInfo.h"
+
 #include OATPP_CODEGEN_BEGIN(DTO)
 
 /*
 	家长数据传输对象
  */
-
-
-
- // PageDTO是模板类，用于分页数据
 class ParentDTO : public oatpp::DTO {
-  DTO_INIT(ParentDTO, DTO);
+	DTO_INIT(ParentDTO, DTO);
 };
-// 自动包含字段：
-// - pageIndex: 当前页码
-// - pageSize: 每页条数
-// - total: 总记录数
-// - pages: 总页数
-// - rows: 数据列表 List<SampleDTO::Wrapper>
-
-
-
-//使用说明列表中的单条数据DTO
-class UsageGuideDto : public oatpp::DTO {
-	DTO_INIT(UsageGuideDto, DTO)
-	//API_DTO_FIELD(Int32, id, "id", true, 0);
-	API_DTO_FIELD(String, title, ZH_WORDS_GETTER("help.title"), true, "");
-	//API_DTO_FIELD(String, add_time, "add_time", true, "");
-	//API_DTO_FIELD(String, edit_time, "edit_time", true, "");
-};
-
-//使用说明详情
-class UsageGuideDetailDto : public oatpp::DTO {
-	DTO_INIT(UsageGuideDetailDto, DTO)
-	//API_DTO_FIELD(Int32, id, "id", true, 0);
-	API_DTO_FIELD(String, title, ZH_WORDS_GETTER("help.title"), true, "");
-	API_DTO_FIELD(String, content, ZH_WORDS_GETTER("help.content"), true, "");
-	//API_DTO_FIELD(String, type, "type", true, "");
-	/*API_DTO_FIELD(Int32, creator, "creator", true,0);
-	API_DTO_FIELD(String, add_time, "add_time", true, "");
-	API_DTO_FIELD(Int32, deleted, "deleted", true, 0);
-	API_DTO_FIELD(String, edit_time, "edit_time", true, "");
-	API_DTO_FIELD(Int32, editor, "editor", true, 0);
-	API_DTO_FIELD(Int32, state, "state", true, 0);
-	API_DTO_FIELD(Int32, sort_num, "sort_num", true, 0);*/
-};
-
-/**
-* 分页DTO
-* 用于：GET /users 接口（分页查询）
-*/
-
-//分页展示列表信息
-class UsageListDTO : public PageDTO<UsageGuideDto::Wrapper>
-{
-	DTO_INIT(UsageListDTO, PageDTO<UsageGuideDto::Wrapper>);
-};
-
-//分页展示列表中某一详细信息
-class UsagePageDTO : public PageDTO<UsageGuideDetailDto::Wrapper>
-{
-	DTO_INIT(UsagePageDTO, PageDTO<UsageGuideDetailDto::Wrapper>);
-};
-
-
-
-
-
-
-
-
-
-//
-////使用说明分页结果DTO
-//class UsageGuidePageDto : public oatpp::DTO {
-//	DTO_INIT(UsageGuidePageDto, DTO)
-//	API_DTO_FIELD(Int32, total, "total", true, 0);
-//	API_DTO_FIELD(Int32, pageIndex, "pageIndex", true, 0);
-//	API_DTO_FIELD(Int32, pageSize, "pageSize", true, 0);
-//	API_DTO_FIELD(Vector<Object<UsageGuideDto>>, list, "datalist", true, {});
-//};
-//
-
-
 
 #include OATPP_CODEGEN_END(DTO)
 #endif // _PARENTDTO_H_
