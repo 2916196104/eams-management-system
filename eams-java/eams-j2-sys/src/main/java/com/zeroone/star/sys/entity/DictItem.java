790a8d9d05a4fff14ef3bd2398667bb946d1e986
火山 <13215761+huo-shan@user.noreply.gitee.com>
2026-03-27 15:46:56 +0800

    【数据字典】【倾枫，cc，开以结果】修正格式，将业务逻辑写到seviceimpl中

--- eams-java/eams-j2-sys/src/main/java/com/zeroone/star/sys/entity/DictItem.java
+++ eams-java/eams-j2-sys/src/main/java/com/zeroone/star/sys/entity/DictItem.java
@@ -3,11 +3,6 @@ package com.zeroone.star.sys.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
-import io.swagger.annotations.ApiModelProperty;
-
-import javax.validation.constraints.NotBlank;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,35 +23,25 @@ public class DictItem implements Serializable {
      * 序列化ID
      */
     private static final long serialVersionUID = 1L;
-
     /**
      * ID
      */
-    @ApiModelProperty(value = "ID", example = "1")
     @TableId(type = IdType.AUTO)
     private Long id;
     /**
      * 数据字典ID
      */
-
-    @ApiModelProperty(value = "数据字典ID", example = "1")
     private Long dictId;
     /**
      * 数据字典名称
      */
-    @NotBlank(message = "数据字典名称不能为空")
-    @ApiModelProperty(value = "数据字典名称", example = "名称")
     private String name;
     /**
      * 数据字典说明
      */
-
-    @ApiModelProperty(value = "数据字典说明", example = "说明")
     private String info;
     /**
      * 数据字典排序
      */
-
-    @ApiModelProperty(value = "数据字典排序", example = "1")
     private Integer sortNum;
 }
