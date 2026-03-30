0f9a6885198cb7b32398d1ac2badaf0275092dca
Ringo0115 <2752649921@qq.com>
2026-03-30 12:17:03 +0800

    package进度保存

--- eams-java/eams-apis/src/main/java/com/zeroone/star/project/j4/academic/AcademicApis.java
+++ eams-java/eams-apis/src/main/java/com/zeroone/star/project/j4/academic/AcademicApis.java
@@ -2,19 +2,15 @@ package com.zeroone.star.project.j4.academic;
 
 import com.zeroone.star.project.dto.PageDTO;
 import com.zeroone.star.project.vo.JsonVO;
+import com.zeroone.star.project.dto.j4.student.StudentDTO;
+import com.zeroone.star.project.query.j4.student.StudentQuery;
+import java.util.List;
 
 /**
  * <p>
  * 描述：学员模块缴欠费与退费接口
  * </p>
  */
-
-import com.zeroone.star.project.dto.PageDTO;
-import com.zeroone.star.project.dto.j4.student.StudentDTO;
-import com.zeroone.star.project.query.j4.student.StudentQuery;
-import com.zeroone.star.project.vo.JsonVO;
-
-import java.util.List;
 
 // 学员相关接口
 public interface AcademicApis {
