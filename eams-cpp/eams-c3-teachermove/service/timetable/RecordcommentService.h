3b278458dc53bba48661a499535bee24abc00fc2
zjh <1658080402@qq.com>
2026-04-02 00:01:06 +0800

    提交小更改

--- eams-cpp/eams-c3-teachermove/service/timetable/RecordcommentService.h
+++ eams-cpp/eams-c3-teachermove/service/timetable/RecordcommentService.h
@@ -14,19 +14,3 @@ public:
 };
 
 #endif
-
-
-#ifndef __RECORDCOMMENT_SERVICE_H__
-#define __RECORDCOMMENT_SERVICE_H__
-
-#include "domain/GlobalInclude.h"
-#include "domain/query/timetable/TimetableQuery.h"
-#include "domain/dto/timetable/TimetableDTO.h"
-
-class RecordCommentService
-{
-public:
-	EvaluatePageDTO::Wrapper getEvaluationList(const EvaluationQuery::Wrapper& query);
-};
-
-#endif
