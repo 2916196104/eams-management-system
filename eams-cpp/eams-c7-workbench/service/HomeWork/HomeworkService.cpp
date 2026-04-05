d320a75eddd4fe9217e5a1b6d79e0574182fd562
Kosma <2257497695@qq.com>
2026-04-05 12:55:51 +0800

    修复了文件头文件与路径大小写不严格一致的问题

--- eams-cpp/eams-c7-workbench/service/HomeWork/HomeworkService.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "HomeworkService.h"
-#include "dao/Homework/HomeworkDAO.h"
-#include "dao/Homework/HomeworkRecordDAO.h"
-#include "domain/do/Homework/HomeworkDO.h"
-#include "domain/do/Homework/HomeworkRecordDO.h"
-#include "id/UuidFacade.h"
-#include "SimpleDateTimeFormat.h"
-
