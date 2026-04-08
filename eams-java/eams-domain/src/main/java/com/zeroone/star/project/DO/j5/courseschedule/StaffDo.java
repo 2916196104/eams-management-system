1cae5563bbc6f5c7147d96e75493162fe28b8239
1517196417 <1517196417@qq.com>
2026-04-08 09:51:48 +0800

    feat: 更新StaffDO实体类

--- eams-java/eams-domain/src/main/java/com/zeroone/star/project/DO/j5/courseschedule/StaffDo.java
+++ eams-java/eams-domain/src/main/java/com/zeroone/star/project/DO/j5/courseschedule/StaffDo.java
@@ -1,68 +1,165 @@
 package com.zeroone.star.project.DO.j5.courseschedule;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+/**
+ * 教师员工表 DO
+ */
 @Data
-@ApiModel(description = "教师DO")
-public class StaffDo {
-    @ApiModelProperty(value = "ID")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("staff")
+public class StaffDO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键ID
+     */
+    @TableId(type = IdType.AUTO)
     private Long id;
-    @ApiModelProperty(value = "姓名")
+
+    /**
+     * 姓名
+     */
     private String name;
-    @ApiModelProperty(value = "手机号")
+
+    /**
+     * 手机号
+     */
     private String mobile;
-    @ApiModelProperty(value = "微信ID")
+
+    /**
+     * 微信登录id
+     */
     private Long wxAccessId;
-    @ApiModelProperty(value = "邮箱")
+
+    /**
+     * EMAIL
+     */
     private String email;
-    @ApiModelProperty(value = "生日")
+
+    /**
+     * 生日
+     */
     private LocalDate birthday;
-    @ApiModelProperty(value = "性别")
+
+    /**
+     * 性别 (0等具体含义视业务而定)
+     */
     private Integer gender;
-    @ApiModelProperty(value = "身份证号")
+
+    /**
+     * 身份证
+     */
     private String idCard;
-    @ApiModelProperty(value = "毕业院校")
+
+    /**
+     * 毕业学校
+     */
     private String school;
-    @ApiModelProperty(value = "学历")
+
+    /**
+     * 学历
+     */
     private Integer degree;
-    @ApiModelProperty(value = "备注")
+
+    /**
+     * 备注
+     */
     private String remark;
-    @ApiModelProperty(value = "状态")
+
+    /**
+     * 在职状态 (默认 1)
+     */
     private Integer state;
-    @ApiModelProperty(value = "是否管理员")
-    private Integer isManager;
-    @ApiModelProperty(value = "头像")
+
+    /**
+     * 是否是管理者 (默认 0)
+     */
+    private Boolean isManager;
+
+    /**
+     * 头像
+     */
     private String headImg;
-    @ApiModelProperty(value = "入职日期")
+
+    /**
+     * 入职日期
+     */
     private LocalDate hireDate;
-    @ApiModelProperty(value = "离职日期")
+
+    /**
+     * 离职日期
+     */
     private LocalDate fireDate;
-    @ApiModelProperty(value = "简介")
+
+    /**
+     * 介绍
+     */
     private String intro;
-    @ApiModelProperty(value = "创建时间")
+
+    /**
+     * 创建时间
+     */
     private LocalDateTime addTime;
-    @ApiModelProperty(value = "编辑时间")
+
+    /**
+     * 编辑时间
+     */
     private LocalDateTime editTime;
-    @ApiModelProperty(value = "创建人")
+
+    /**
+     * 创建人
+     */
     private Long creator;
-    @ApiModelProperty(value = "编辑人")
+
+    /**
+     * 编辑人
+     */
     private Long editor;
-    @ApiModelProperty(value = "删除标记")
-    private Integer deleted;
-    @ApiModelProperty(value = "密码")
+
+    /**
+     * 是否删除 (0未删, 1已删)
+     */
+    @TableLogic
+    private Boolean deleted;
+
+    /**
+     * 密码
+     */
     private String password;
-    @ApiModelProperty(value = "授课费")
+
+    /**
+     * 课时费
+     */
     private BigDecimal classFee;
-    @ApiModelProperty(value = "助教费")
+
+    /**
+     * 助教费
+     */
     private BigDecimal assistantFee;
-    @ApiModelProperty(value = "机构ID")
+
+    /**
+     * STAFF所属组织ID
+     */
     private Long orgId;
-    @ApiModelProperty(value = "是否内部员工")
-    private Integer isInner;
+
+    /**
+     * 是否内部人员 (默认 1)
+     */
+    private Boolean isInner;
 }
