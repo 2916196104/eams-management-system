6a733b295bd768577a1f2e82357559171b29484a
Ding <13579@qq.com>
2026-04-08 18:27:06 +0800

    部署更新

--- eams-java/eams-j4-academic/src/main/java/com/zeroone/star/academic/AcademicApplication.java
+++ eams-java/eams-j4-academic/src/main/java/com/zeroone/star/academic/AcademicApplication.java
@@ -1,14 +1,14 @@
 package com.zeroone.star.academic;
 
-import io.seata.spring.boot.autoconfigure.SeataAutoConfiguration;
+//import io.seata.spring.boot.autoconfigure.SeataAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+//@EnableFeignClients
 public class AcademicApplication {
     public static void main(String[] args) {
         SpringApplication.run(AcademicApplication.class, args);
